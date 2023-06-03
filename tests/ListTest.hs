dba211e1b48c84a613c1cba7a11e01804c4d62ee
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-06-03 14:13:59 +0200

    More tests

--- tests/ListTest.hs
+++ tests/ListTest.hs
@@ -1,12 +1,11 @@
-module ListTest(Module ListTest) where
+module ListTest(module ListTest) where
 import Prelude
 import Data.List
 import System.IO
+import String
 
 main = do
-  print (sum [1,2,3])
-  print sum
-  print (and [True])
-  print and
-  print (&&)
-  
+  putStrLn $ showInt $ sum [1,2,3]
+  putStrLn $ showInt $ product [1,2,3,4]
+  putStrLn $ showBool $ and [True]
+  putStrLn $ showBool $ and [True, False]
