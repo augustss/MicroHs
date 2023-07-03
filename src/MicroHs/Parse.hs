d6c25f00c007599b2a0621f54bdd7e4d9a788022
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-07-03 12:23:44 +0200

    Make ParserComb compile with uhs.

--- src/MicroHs/Parse.hs
+++ src/MicroHs/Parse.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-type-defaults -Wno-name-shadowing #-}
+{-# LANGUAGE QualifiedDo #-}
 module MicroHs.Parse(
   pTop,
   parseDie,
@@ -14,11 +16,13 @@ module MicroHs.Parse(
   ExportSpec(..),
   LHS,
   ) where
-import Control.Monad
-import Control.Monad.State.Strict
+import Prelude hiding (Monad(..), Applicative(..), MonadFail(..), Functor(..), (<$>))
+--import Control.Monad
+--import Control.Monad.State.Strict
+--import Control.Applicative --hiding (many, some)
+import Data.Char
 import Data.List
-import Text.ParserComb
-import Control.Applicative --hiding (many, some)
+import Text.ParserComb as P
 --import Debug.Trace
 
 
@@ -90,7 +94,7 @@ skipWhite' p = p <* emany (char ' ')
 -- Skip white-space.
 -- If there is a newline, return the indentation of the last line.
 pIndent :: P (Maybe String)
-pIndent = do
+pIndent = P.do
   s <- emany (satisfy "white-space" (`elem` " \n\r"))
   let s' = takeWhile (== ' ') $ reverse s
   if s == s' then
@@ -99,23 +103,23 @@ pIndent = do
     pure $ Just s'
 
 pWhite :: P ()
-pWhite = do
+pWhite = P.do
   msp <- pIndent
   case msp of
     Nothing -> pure ()
-    Just sp -> do
+    Just sp -> P.do
       st <- get
       case st of
         [] -> pure ()
-        i : is -> do
+        i : is -> P.do
           let
             c = length sp
-          if c < i then do
+          if c < i then P.do
             inject ('}' : '\n' : sp)
             put is
            else if c > i then
             pure ()
-           else do
+           else P.do
             eof <|> inject ";"
 
 esepBy1 :: Prsr s a -> Prsr s sep -> Prsr s [a]
@@ -133,8 +137,58 @@ parseDie p fn file =
 
 -- Remove comments first instead of having them in the parser.
 removeComments :: String -> String
-removeComments = remCom
-  where remCom ('-':'-':cs) = skipToNL cs
+removeComments =
+  let
+    remCom acs =
+      case stripPrefix "--" acs of
+        Just cs -> skipToNL cs
+        Nothing ->
+          case stripPrefix "{-" acs of
+            Just cs -> skipBlock 1 cs
+            Nothing ->
+              case acs of
+                [] -> ""
+                c:cs ->
+                  if c == '"' then '"' : str cs
+                  else if c == '\'' then '\'' : chr cs
+                  else c : remCom cs
+    str acs =
+      case acs of
+        [] -> ""
+        c:cs ->
+          if c == '"' then '"' : remCom cs
+          else if c == '\\' then '\\':head cs:str (tail cs)
+          else c : str cs
+    chr acs =
+      case acs of
+        [] -> ""
+        c:cs ->
+          if c == '\'' then '\'' : remCom cs
+          else if c == '\\' then '\\':head cs:chr (tail cs)
+          else c : chr cs
+    skipToNL acs =
+      case acs of
+        [] -> ""
+        c:cs ->
+          if c == '\n' then '\n' : remCom cs
+          else skipToNL cs
+    skipBlock n acs =
+      if n == 0 then
+        remCom acs
+      else
+        case stripPrefix "{-" acs of
+          Just cs -> skipBlock (n+1) cs
+          Nothing ->
+            case stripPrefix "-}" acs of
+              Just cs -> skipBlock (n-1) cs
+              Nothing ->
+                case acs of
+                  [] -> ""
+                  c:cs -> if c == '\n' then '\n' : skipBlock n cs
+                          else skipBlock n cs
+  in  remCom
+{-
+        remCom ('-':'-':cs) = skipToNL cs
         remCom ('{':'-':cs) = skipBlock 1 cs
         remCom ('"':cs) = '"':str cs
         remCom ('\'':cs) = '\'':chr cs
@@ -158,9 +212,10 @@ removeComments = remCom
         skipBlock n ('\n':cs) = '\n' : skipBlock n cs
         skipBlock n (_:cs) = skipBlock n cs
         skipBlock _ "" = ""
+-}
 
 -------
-
+{-
 isLower :: Char -> Bool
 isLower c = 'a' <= c && c <= 'z'
 
@@ -172,7 +227,7 @@ isLetter c = isLower c || isUpper c
 
 isDigit :: Char -> Bool
 isDigit c = '0' <= c && c <= '9'
-
+-}
 -------
 
 pTop :: P EModule
@@ -187,21 +242,21 @@ pExportSpec :: P ExportSpec
 pExportSpec = ExpModule <$> (pKeyword "module" *> pUIdent)
 
 pKeyword :: String -> P ()
-pKeyword kw = (skipWhite $ do
+pKeyword kw = (skipWhite $ P.do
   s <- pWord
   guard (kw == s)
   pure ()
   ) <?> kw
 
 pKeyword' :: String -> P ()
-pKeyword' kw = (skipWhite' $ do
+pKeyword' kw = (skipWhite' $ P.do
   s <- pWord
   guard (kw == s)
   pure ()
   ) <?> kw
 
 pLIdentA :: P String
-pLIdentA = skipWhite $ do
+pLIdentA = skipWhite $ P.do
   s <- pQIdent
   guard $ isLower $ head s
   pure s
@@ -213,7 +268,7 @@ pLIdent_ :: P String
 pLIdent_ = pLIdent <|> skipWhite (string "_")
 
 pUIdentA :: P String
-pUIdentA = skipWhite $ do
+pUIdentA = skipWhite $ P.do
   s <- pQIdent
   guard $ isUpper $ head s
   pure s
@@ -232,7 +287,7 @@ pWord = (:) <$> satisfy "letter" isLetter <*>
                     c == '_' || c == '\'')
 
 pIdent :: P String
-pIdent = do
+pIdent = P.do
   s <- pWord
   guard (s `notElem` keywords)
   pure s
@@ -245,7 +300,7 @@ pInt = (read <$> (skipWhite $ esome $ satisfy "digit" isDigit)) <?> "int"
 
 pChar :: P Char
 pChar = skipWhite (char '\'' *> pc <* char '\'')
-  where pc = do
+  where pc = P.do
           c <- satisfy "char" (/= '\'')
           if c == '\\' then
             decodeChar <$> satisfy "char" (const True)
@@ -254,7 +309,7 @@ pChar = skipWhite (char '\'' *> pc <* char '\'')
 
 pString :: P String
 pString = skipWhite (char '"' *> emany pc <* char '"')
-  where pc = do
+  where pc = P.do
           c <- satisfy "char" (/= '"')
           guard (c /= '\n')
           if c == '\\' then
@@ -267,7 +322,7 @@ decodeChar 'n' = '\n'
 decodeChar c = c
 
 pSymbol :: String -> P ()
-pSymbol s = (do
+pSymbol s = (P.do
   s' <- pOper'
   guard (s == s')
   pure ()
@@ -277,25 +332,25 @@ pOper' :: P String
 pOper' = skipWhite $ esome $ satisfy "symbol" (`elem` "@\\=+-:<>.!#$%^&*/|~?")
 
 pOper :: P String
-pOper = do
+pOper = P.do
   s <- pOper'
   guard $ s `notElem` ["=", "|", "::", "<-", "@"]
   pure s
 
 pOperL :: P String
-pOperL = do
+pOperL = P.do
   s <- pOper
   guard $ head s /= ':'
   pure s
 
 pOperU :: P String
-pOperU = do
+pOperU = P.do
   s <- pOper
   guard $ head s == ':'
   pure s
 
 pOpers :: [String] -> P String
-pOpers ops = do
+pOpers ops = P.do
   op <- pOper
   guard (op `elem` ops)
   pure op
@@ -339,7 +394,7 @@ pAExpr =
   <|> (ECompr <$> (pSym '[' *> pExpr <* pSym '|') <*> (esepBy1 pStmt (pSym ',') <* pSym ']'))
 
 pExprApp :: P Expr
-pExprApp = do
+pExprApp = P.do
   f <- pAExpr
   as <- emany pAExpr
   pure $ foldl EApp f as
@@ -368,38 +423,41 @@ pExprOp = p0
     p0 = pRightAssoc (pOpers ["$", "->"]) p1   -- XXX where should -> be?
     p1 = pLeftAssoc  (pOpers [">>=", ">>"]) p2
     p2 = pRightAssoc (pOpers ["||"]) p3
-    p3 = pRightAssoc (pOpers ["&&"]) p4
-    p4 = pNonAssoc   (pOpers ["==", "/=", "<", "<=", ">", ">="]) p5
+    p3 = pRightAssoc (pOpers ["&&"]) $
+         pLeftAssoc  (pOpers ["<|>","<|<"]) p4
+    p4 = pNonAssoc   (pOpers ["==", "/=", "<", "<=", ">", ">="]) $
+         pLeftAssoc  (pOpers ["<*>", "<*", "*>", "<$>", "<$"])   p5
     p5 = pRightAssoc (pOpers [":", "++"]) p6
     p6 = pLeftAssoc  (pOpers ["+", "-"]) p7
     p7 = pLeftAssoc  (pOpers ["*", "quot", "rem"]) p8
     p8 = p9
-    p9 = pRightAssoc (pOpers ["."]) p10
+    p9 = pRightAssoc (pOpers ["."]) $
+         pLeftAssoc  (pOpers ["?"]) p10
     p10 = pExprArg
 
 appOp :: String -> Expr -> Expr -> Expr
 appOp op e1 e2 = EApp (EApp (EVar op) e1) e2
 
 pRightAssoc :: P String -> P Expr -> P Expr
-pRightAssoc pOp p = do
+pRightAssoc pOp p = P.do
   e1 <- p
-  let rest = do
+  let rest = P.do
         op <- pOp
         e2 <- pRightAssoc pOp p
         pure $ appOp op e1 e2
   rest <|< pure e1
 
 pNonAssoc :: P String -> P Expr -> P Expr
-pNonAssoc pOp p = do
+pNonAssoc pOp p = P.do
   e1 <- p
-  let rest = do
+  let rest = P.do
         op <- pOp
         e2 <- p
         pure $ appOp op e1 e2
   rest <|< pure e1
 
 pLeftAssoc :: P String -> P Expr -> P Expr
-pLeftAssoc pOp p = do
+pLeftAssoc pOp p = P.do
   e1 <- p
   es <- emany ((,) <$> pOp <*> p)
   pure $ foldl (\ x (op, y) -> appOp op x y) e1 es
@@ -428,7 +486,7 @@ pBind =
   <|> BPat <$> (pPat <* pSymbol "=") <*> pExpr
 
 pQualDo :: P String
-pQualDo = do
+pQualDo = P.do
   s <- pUIdent
   _ <- char '.'
   pKeyword' "do"
@@ -436,26 +494,26 @@ pQualDo = do
 
 pLCurl :: P ()
 pLCurl = pSym '{' <|< softLC
-  where softLC = do
+  where softLC = P.do
           msp <- pIndent
           case msp of
             Nothing -> fail "\\n"
-            Just sp -> do
+            Just sp -> P.do
 --              traceM ("push " ++ show (length sp))
               modify $ \ st -> length sp : st
 
 pRCurl :: P ()
 pRCurl = pSym '}' <|> softRC
-  where softRC = do
+  where softRC = P.do
           is <- get
           if null is then
             fail "}"
-           else do
+           else P.do
             put (tail is)
             eof
 
 pBlock :: P a -> P [a]
-pBlock p = do
+pBlock p = P.do
   pLCurl
   as <- esepBy p (pSym ';')
   pRCurl
--- src/Text/ParserComb.hs
+++ src/Text/ParserComb.hs
@@ -1,6 +1,9 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Text.ParserComb(
+  module Text.ParserComb
+{-
   get, gets, put, modify,
   Prsr, runPrsr,
   satisfy, char, string, eof,
@@ -10,78 +13,149 @@ module Text.ParserComb(
   (<?>), (<|<),
   notFollowedBy, lookAhead,
   inject,
+-}
   )where
+import Prelude --X hiding (Monad(..), MonadFail(..), Applicative(..), Functor(..), (<$>), showString)
+{-
 import Control.Monad
 import Control.Monad.State.Strict
-import Data.Char(isSpace)
+import Control.Applicative --hiding (many, some)
+-}
+--import Data.Char
 import Data.List
 --import Debug.Trace
 
-import Control.Applicative --hiding (many, some)
-
 data LastFail
   = LastFail Int [(String, [String])]
-  deriving (Show)
+  --deriving (Show)
+
+--XshowString :: String -> String
+--XshowString = show
+--XshowInt :: Int -> String
+--XshowInt = show
+
+maxInt :: Int
+maxInt = 1000000000
 
 noFail :: LastFail
-noFail = LastFail maxBound []
+noFail = LastFail maxInt []
 
 longest :: LastFail -> LastFail -> LastFail
-longest lf1@(LastFail l1 x1) lf2@(LastFail l2 x2) =
-  case compare l1 l2 of
-    LT -> lf1
-    GT -> lf2
-    EQ -> LastFail l1 (x1 ++ x2)
+longest lf1 lf2 =
+  case lf1 of
+    LastFail l1 x1 ->
+      case lf2 of
+        LastFail l2 x2 ->
+          if l1 < l2 then
+            lf1
+          else if l2 < l1 then
+            lf2
+          else
+            LastFail l1 (x1 ++ x2)
 
 longests :: [LastFail] -> LastFail
-longests [] = undefined
-longests xs = foldl1 longest xs
+longests xs =
+  case xs of
+    [] -> undefined
+    _:_ -> foldl1 longest xs
 
 data Res s a = Many [(a, (String, s))] LastFail
-  deriving (Show)
+  --deriving (Show)
 
-newtype Prsr s a = P { runP :: (String, s) -> Res s a}
-instance Show (Prsr s a) where show _ = "<<Prsr>>"
+data Prsr s a = P ((String, s) -> Res s a)
+runP :: Prsr s a -> ((String, s) -> Res s a)
+runP pp =
+  case pp of
+    P p -> p
+--instance Show (Prsr s a) where show _ = "<<Prsr>>"
 
-instance Functor (Prsr s) where
-  fmap f p = P $ \ t ->
-    case runP p t of
-      Many aus lf -> Many [ (f a, u) | (a, u) <- aus ] lf
+pure :: a -> Prsr s a
+pure a = P $ \ t -> Many [(a, t)] noFail
 
-instance Applicative (Prsr s) where
-  (<*>) = ap
-  pure a = P $ \ t -> Many [(a, t)] noFail
-
-instance Monad (Prsr s) where
-  return = pure
-  p >>= k = P $ \ t ->
+--Xinfixl 1 >>=
+(>>=) :: Prsr s a -> (a -> Prsr s b) -> Prsr s b
+(>>=) p k = P $ \ t ->
     case runP p t of
       Many aus plf ->
-        let xss = [ runP (k a) u | (a, u) <- aus ]
-        in  let (rss, lfs) = unzip [ (rs, lf) | Many rs lf <- xss ]
-            in  Many (concat rss) (longests $ plf : lfs)
+        let { xss = [ runP (k a) u | au <- aus, let { (a, u) = au } ] }
+        in  case unzip [ (rs, lf) | xs <- xss, let { Many rs lf = xs } ] of
+              (rss, lfs) -> Many (concat rss) (longests $ plf : lfs)
 
-instance MonadFail (Prsr s) where
-  fail m = P $ \ (r, _) -> Many [] (LastFail (length r) [("fail", [m])])
+-- XXX needs (x,y) <- e
 
-instance MonadState s (Prsr s) where
-  get = P $ \ t@(_, s) -> Many [(s, t)] noFail
-  put s = P $ \ (f, _) -> Many [((), (f, s))] noFail
+--Xinfixl 1 >>
+(>>) :: Prsr s a -> Prsr s b -> Prsr s b
+(>>) p k = p >>= \ _ -> k
 
-instance Alternative (Prsr s) where
-  empty = P $ \ (r, _) -> Many [] (LastFail (length r) [("empty", ["<empty>"])])
-  p <|> q = P $ \ t ->
-    case (runP p t, runP q t) of
-      (Many a lfa, Many b lfb) -> Many (a ++ b) (longest lfa lfb)
+--Xinfixl 4 <*>
+(<*>) :: Prsr s (a -> b) -> Prsr s a -> Prsr s b
+(<*>) m1 m2 = m1 >>= \ x1 -> m2 >>= \ x2 -> pure (x1 x2)
+
+--Xinfixl 4 <*
+(<*) :: Prsr s a -> Prsr s b -> Prsr s a
+(<*) m1 m2 = m1 >>= \ x1 -> m2 >> pure x1
+
+--Xinfixl 4 *>
+(*>) :: Prsr s a -> Prsr s b -> Prsr s b
+(*>) m1 m2 = m1 >> m2 >>= \ x2 -> pure x2
+
+--Xinfixl 4 <$>
+(<$>) :: (a -> b) -> Prsr s a -> Prsr s b
+(<$>) f p = P $ \ t ->
+    case runP p t of
+      Many aus lf -> Many [ (f a, u) | au <- aus, let { (a, u) = au } ] lf
+
+--Xinfixl 4 <$
+(<$) :: a -> Prsr s b -> Prsr s a
+(<$) a p = p >> pure a
+
+guard :: Bool -> Prsr s ()
+guard b = if b then pure () else empty
+
+empty :: Prsr s a
+empty = P $ \ t -> Many [] (LastFail (length (fst t)) [("empty", ["<empty>"])])
+
+--Xinfixl 3 <|>
+(<|>) :: Prsr s a -> Prsr s a -> Prsr s a
+(<|>) p q = P $ \ t ->
+    case runP p t of
+      Many a lfa ->
+        case runP q t of
+          Many b lfb -> Many (a ++ b) (longest lfa lfb)
+
+fail :: String -> Prsr s a
+fail m = P $ \ t -> Many [] (LastFail (length (fst t)) [("fail", [m])])
+
+get :: Prsr s s
+get = P $ \ t -> Many [(snd t, t)] noFail
+
+put :: s -> Prsr s ()
+put s = P $ \ t -> Many [((), (fst t, s))] noFail
+
+modify :: (s -> s) -> Prsr s ()
+modify f = get >>= \ s -> put (f s)
 
 -- Left biased choice
 (<|<) :: Prsr s a -> Prsr s a -> Prsr s a
-p <|< q = P $ \ t ->
-  case runP p t of
-    Many [] lfa ->
-      case runP q t of
-        Many b lfb -> Many b (longest lfa lfb)
-    r -> r
+(<|<) p q = P $ \ t ->
+  let
+    r = runP p t
+  in case r of
+       Many rs lfa ->
+         case rs of
+           [] ->
+             case runP q t of
+               Many b lfb -> Many b (longest lfa lfb)
+           _:_ -> r
+
+many :: Prsr s a -> Prsr s [a]
+many p = some p <|> pure []
+
+some :: Prsr s a -> Prsr s [a]
+some p = (:) <$> p <*> many p
+
+optional :: Prsr s a -> Prsr s (Maybe a)
+optional p = (Just <$> p) <|> pure Nothing
 
 emany :: Prsr s a -> Prsr s [a]
 emany p = esome p <|< pure []
@@ -92,72 +166,108 @@ esome p = (:) <$> p <*> emany p
 eoptional :: Prsr s a -> Prsr s (Maybe a)
 eoptional p = (Just <$> p) <|< pure Nothing
 
-runPrsr :: (Show a, Show s) => s -> Prsr s a -> FilePath -> String -> Either String [(a, s)]
-runPrsr s (P p) fn f =
-  case p (f, s) of
-    Many [] lf -> Left $ formatFailed fn f lf
-    Many xs _  -> Right [(a, s') | (a, (_, s')) <- xs ]
+--XrunPrsr :: (Show a, Show s) => s -> Prsr s a -> FilePath -> String -> Either String [(a, s)]
+runPrsr s pp fn f =
+  case pp of
+    P p ->
+      case p (f, s) of
+        Many xs lf ->
+          case xs of
+            []  -> Left $ formatFailed fn f lf
+            _:_ -> Right [(a, snd x) | ax <- xs, let { (a, x) = ax } ]
 
 formatFailed :: FilePath -> String -> LastFail -> String
-formatFailed fn file (LastFail len xs) | len == maxBound = "No failure"
-                                    | otherwise =
-  let (pre, post) = splitAt (length file - len) file
-      (line, col) = foldl f (1::Int, 0::Int) pre
-        where f (l, _) '\n' = (l+1, 0)
-              f (l, c) _    = (l, c+1)
-      xs' = nub $ map trim xs
-      pr e = "   expeced: " ++ e
-      trim (_xx, es) = unwords es -- (last $ init $ "" : "" : es)
+formatFailed fn file lf =
+  case lf of
+    LastFail len _ ->
+      let
+        (pre, post) = splitAt (length file - len) file
+        count lc x =
+          case lc of
+            (l, c) ->
+              if x == '\n' then (l+1, 0) else (l, c+1)
+        (line, col) = foldl count (1, 0) pre
+      in showString fn ++ ": " ++
+         "line " ++ showInt line ++ ", col " ++ showInt col ++ ":\n" ++
+         "   found: " ++ showString (take 10 post)
+{-
+    xs' = nub $ map trim xs
+    pr e = "   expeced: " ++ e
+    trim arg = unwords (snd arg) -- (last $ init $ "" : "" : es)
   in  show fn ++ ": " ++
       "line " ++ show line ++ ", col " ++ show col ++ ":\n" ++
       "   found: " ++ show (takeWhile (not . isSpace) post) ++ "\n" ++
       unlines (map pr xs')
+-}
 
 choice :: [Prsr s a] -> Prsr s a
-choice [] = empty
-choice ps = foldr1 (<|>) ps
+choice ps =
+  case ps of
+    [] -> empty
+    _:_ -> foldr1 (<|>) ps
 
 satisfy :: String -> (Char -> Bool) -> Prsr s Char
 satisfy msg f = P $ \ t ->
   case t of
-    (c:cs, s) | f c -> Many [(c, (cs, s))] noFail
-    (cs, _) -> Many [] (LastFail (length cs) [("satisfy", [msg])])
+    (acs, s) ->
+      let
+        bad = Many [] (LastFail (length acs) [("satisfy", [msg])])
+      in
+        case acs of
+          [] -> bad
+          c:cs ->
+            if f c then
+              Many [(c, (cs, s))] noFail
+            else
+              bad
 
 char :: Char -> Prsr s Char
-char c = P $ \ t ->
-  case t of
-    (c':cs, s) | c == c' -> Many [(c, (cs, s))] noFail
-    (cs, _) -> Many [] (LastFail (length cs) [("char", [show c])])
+char c = satisfy "char" (== c)
 
 string :: String -> Prsr s String
 string str = P $ \ t ->
   case t of
-    (cs, s) | Just cs' <- stripPrefix str cs -> Many [(str, (cs', s))] noFail
-            | otherwise -> Many [] (LastFail (length cs) [("string", [show str])])
+    (cs, s) ->
+      case stripPrefix str cs of
+        Just cs' -> Many [(str, (cs', s))] noFail
+        Nothing  -> Many [] (LastFail (length cs) [("string", [showString str])])
 
 eof :: Prsr s ()
 eof = P $ \ t ->
-  case t of
-    ("", _) -> Many [((), t)] noFail
-    (cs, _) -> Many [] (LastFail (length cs) [("eof", ["end-of-file"])])
+  let
+    cs = fst t
+  in
+     if null cs then
+       Many [((), t)] noFail
+     else
+       Many [] (LastFail (length cs) [("eof", ["end-of-file"])])
 
 (<?>) :: Prsr s a -> String -> Prsr s a
-p <?> e = P $ \ t ->
+(<?>) p e = P $ \ t ->
 --  trace ("<?> " ++ show e) $
   case runP p t of
-    Many rs (LastFail l xs) -> Many rs (LastFail l [(m, e:es) | (m, es) <- xs ])
+    Many rs lf ->
+      case lf of
+        LastFail l xs -> Many rs (LastFail l [(m, e:es) | mes <- xs, let { (m, es) = mes } ])
 
 notFollowedBy :: Prsr s a -> Prsr s ()
-notFollowedBy p = P $ \ t@(cs,_) ->
+notFollowedBy p = P $ \ t ->
   case runP p t of
-    Many [] _ -> Many [((), t)] noFail
-    Many _ _ -> Many [] (LastFail (length cs) [("notFollowedBy", [])])
+    Many rs _ ->
+      if null rs then
+        Many [((), t)] noFail
+      else
+        Many [] (LastFail (length (fst t)) [("notFollowedBy", [])])
 
 lookAhead :: Prsr s a -> Prsr s ()
 lookAhead p = P $ \ t ->
   case runP p t of
-    Many [] (LastFail l xs) -> Many [] (LastFail l [("lookAhead-" ++ m, es) | (m, es) <- xs])
-    Many _ _ -> Many [((), t)] noFail
+    Many rs lf ->
+      if null rs then
+        case lf of
+          LastFail l xs -> Many [] (LastFail l [("lookAhead-" ++ m, es) | mes <- xs, let { (m, es) = mes }])
+      else
+        Many [((), t)] noFail
 
 inject :: String -> Prsr s ()
-inject s = P $ \ (cs, st) -> Many [((), (s ++ cs, st))] noFail
+inject s = P $ \ csst -> case csst of { (cs, st) -> Many [((), (s ++ cs, st))] noFail }
