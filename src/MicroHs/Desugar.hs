4e2bb77294bbf90cea7d070c8fa609215c892899
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-07-03 11:59:40 +0200

    Refactor symbol table handling.

--- src/MicroHs/Desugar.hs
+++ src/MicroHs/Desugar.hs
@@ -25,12 +25,18 @@ data TypeDef = TypeDef Ident [(Ident, Int)]   -- constructor name, arity
 
 type LDef = (Ident, Exp)
 
-type SymTable = M.Map Ident [Exp]
+type ValTable = M.Map Ident [Exp]
 type TypeTable = M.Map Ident [TypeDef]
+
+data SymTable = SymTable { sVal :: ValTable, sType :: TypeTable }
+  deriving (Show)
+
+extVals :: SymTable -> [Ident] -> SymTable
+extVals (SymTable vals tys) is = SymTable (foldr (\ x -> M.insert x [Var x]) vals is) tys
 
 desugar :: [(ImportSpec, Module)] -> EModule -> Module
 desugar imdls (EModule mdln especs ds) =
-  let ds' = concatMap (dsDef allSyms allTypes) ds
+  let ds' = concatMap (dsDef allSyms) ds
       tyds = concatMap exportT especs
       exportT (ExpModule m) =
         if m == mdln then
@@ -48,86 +54,87 @@ desugar imdls (EModule mdln especs ds) =
       qns (ImportSpec q _ mas) mn i =
         let mn' = fromMaybe mn mas
         in  if q then [qual mn' i] else [i, qual mn' i]
-      allSyms :: SymTable
-      allSyms = M.fromListWith union $ concatMap syms mdls
+      allVals :: ValTable
+      allVals = M.fromListWith union $ concatMap syms mdls
         where syms (is, Module mn qis _ _) = [ (v, [Var qi]) | (i, qi) <- qis, v <- qns is mn i ]
       allTypes :: TypeTable
       allTypes = M.fromListWith union $ concatMap types mdls
         where types (is, Module mn _ tds _) = [ (v, [td]) | td@(TypeDef _ cs) <- tds, (c, _) <- cs, v <- qns is mn c ]
+      allSyms = SymTable { sVal = allVals, sType = allTypes }
   in  mdl
 
-dsDef :: SymTable -> TypeTable -> EDef -> [LDef]
-dsDef _ _ (Data _ cs) = zipWith dsConstr [0..] cs
+dsDef :: SymTable -> EDef -> [LDef]
+dsDef _ (Data _ cs) = zipWith dsConstr [0..] cs
   where
     fs = [f i | (i, _) <- zip [0..] cs]
     dsConstr i (c, ts) = (c, lams xs $ lams fs $ apps (Var (f i)) (map Var xs))
       where xs = ["$x" ++ show (j::Int) | (j, _) <- zip [0..] ts]
     f i = "$f" ++ show (i::Int)
-dsDef _ _ Type{} = []
-dsDef syms tys (Fcn (f, xs) e) = [(f, lams xs $ dsExpr (extSyms syms xs) tys e)]
-dsDef _ _ Sign{} = []
-dsDef _ _ Import{} = []
+dsDef _ Type{} = []
+dsDef syms (Fcn (f, xs) e) = [(f, lams xs $ dsExpr (extVals syms xs) e)]
+dsDef _ Sign{} = []
+dsDef _ Import{} = []
 
-dsBind :: SymTable -> TypeTable -> EBind -> [LDef]
-dsBind syms tys (BFcn (f, xs) e) = [(f, lams xs $ dsExpr (extSyms syms xs) tys e)]
-dsBind syms tys b@(BPat p e) =
+dsBind :: SymTable -> EBind -> [LDef]
+dsBind syms (BFcn (f, xs) e) = [(f, lams xs $ dsExpr (extVals syms xs) e)]
+dsBind syms b@(BPat p e) =
   let v = newVar (allVarsBind b)
-      de = (v, dsExpr syms tys e)
-      ds = [ (i, dsExpr syms tys (ECase (EVar v) [(p, EVar i)])) | i <- patVars p ]
+      de = (v, dsExpr syms e)
+      ds = [ (i, dsExpr syms (ECase (EVar v) [(p, EVar i)])) | i <- patVars p ]
   in  de : ds
 
-dsExpr :: SymTable -> TypeTable -> Expr -> Exp
-dsExpr syms _ (EVar i) =
-  case M.lookup i syms of
+dsExpr :: SymTable -> Expr -> Exp
+dsExpr syms (EVar i) =
+  case M.lookup i (sVal syms) of
     Nothing -> error $ "undefined: " ++ show i
     Just [qi] -> qi
     Just qis -> error $ "ambiguous: " ++ show i ++ ", " ++ show qis
-dsExpr syms tys (EApp f a) = App (dsExpr syms tys f) (dsExpr syms tys a)
-dsExpr syms tys (ELam xs e) = lams xs (dsExpr (extSyms syms xs) tys e)
-dsExpr _ _ (EInt i) = Int (fromInteger i)
-dsExpr _ _ (EChar c) = Int (fromEnum c)
-dsExpr syms tys (ECase e as) = apps (dsExpr syms tys e) (map dsArm as')
-  where dsArm (PConstr _ vs, r) = lams vs $ dsExpr (extSyms syms vs) tys r
+dsExpr syms (EApp f a) = App (dsExpr syms f) (dsExpr syms a)
+dsExpr syms (ELam xs e) = lams xs (dsExpr (extVals syms xs) e)
+dsExpr _ (EInt i) = Int (fromInteger i)
+dsExpr _ (EChar c) = Int (fromEnum c)
+dsExpr syms (ECase e as) = apps (dsExpr syms e) (map dsArm as')
+  where dsArm (PConstr _ vs, r) = lams vs $ dsExpr (extVals syms vs) r
         dsArm (PTuple [_], _) = error "dsExpr: singleton tuple"
-        dsArm (PTuple vs, r) = lams vs $ dsExpr (extSyms syms vs) tys r
-        as' = reorderArms tys as
+        dsArm (PTuple vs, r) = lams vs $ dsExpr (extVals syms vs) r
+        as' = reorderArms (sType syms) as
 -- For now, just sequential bindings; each recursive
-dsExpr syms tys (ELet [] e) = dsExpr syms tys e
-dsExpr syms tys (ELet (d:ds) e) =
-  let ds' = dsBind syms' tys d
-      syms' = extSyms syms (map fst ds')
-      e' = dsExpr syms' tys (ELet ds e)
+dsExpr syms (ELet [] e) = dsExpr syms e
+dsExpr syms (ELet (d:ds) e) =
+  let ds' = dsBind syms' d
+      syms' = extVals syms (map fst ds')
+      e' = dsExpr syms' (ELet ds e)
       def (i, r) a = App (Lam i a) (App (Prim "Y") (Lam i r))
   in  foldr def e' ds'
-dsExpr syms tys (EList es) =
-  foldr (App2 CO) CK $ map (dsExpr syms tys) es
-dsExpr _ _ (ETuple []) = Lam "_x" (Var "_x")    -- encoding of ()
-dsExpr syms tys (ETuple [e]) = dsExpr syms tys e
-dsExpr syms tys (ETuple es) = Lam "_f" $ foldl App (Var "_f") $ map (dsExpr syms tys) es
-dsExpr syms tys (EStr cs) = dsExpr syms tys $ EList $ map EChar cs
-dsExpr _ _ (EDo _ []) = error "empty do"
-dsExpr _ _ (EDo _ [SBind _ _]) = error "do without final expression"
-dsExpr syms tys (EDo _ [SThen e]) = dsExpr syms tys e
-dsExpr syms tys (EDo mn (SBind i e : ss)) =
-  dsExpr syms tys $ EApp (EApp (EVar (mqual mn ">>=")) e) (ELam [i] (EDo mn ss))
-dsExpr syms tys (EDo mn (SThen   e : ss)) =
-  dsExpr syms tys $ EApp (EApp (EVar (mqual mn ">>")) e) (EDo mn ss)
-dsExpr syms tys (EDo mn (SLet ds : ss)) =
-  dsExpr syms tys $ ELet ds (EDo mn ss)
-dsExpr _ _ (EPrim s) = Prim s
-dsExpr syms tys (ESectL e op) =
-  App (dsExpr syms tys (EVar op)) (dsExpr syms tys e)
-dsExpr syms tys (ESectR op e) =
-  App2 CC (dsExpr syms tys (EVar op)) (dsExpr syms tys e)
-dsExpr syms tys (EIf e1 e2 e3) =
-  App2 (dsExpr syms tys e1) (dsExpr syms tys e3) (dsExpr syms tys e2)
-dsExpr syms tys (ECompr e []) = dsExpr syms tys (EList [e])
-dsExpr syms tys (ECompr e (SBind i b : ss)) =
-  App2 (Var "Data.List.concatMap") (dsExpr syms tys (ELam [i] (ECompr e ss))) (dsExpr syms tys b)
-dsExpr syms tys (ECompr e (SThen c : ss)) =
-  dsExpr syms tys (EIf c (ECompr e ss) (EList []))
-dsExpr syms tys (ECompr e (SLet d : ss)) =
-  dsExpr syms tys (ELet d (ECompr e ss))
+dsExpr syms (EList es) =
+  foldr (App2 CO) CK $ map (dsExpr syms) es
+dsExpr _ (ETuple []) = Lam "_x" (Var "_x")    -- encoding of ()
+dsExpr syms (ETuple [e]) = dsExpr syms e
+dsExpr syms (ETuple es) = Lam "_f" $ foldl App (Var "_f") $ map (dsExpr syms) es
+dsExpr syms (EStr cs) = dsExpr syms $ EList $ map EChar cs
+dsExpr _ (EDo _ []) = error "empty do"
+dsExpr _ (EDo _ [SBind _ _]) = error "do without final expression"
+dsExpr syms (EDo _ [SThen e]) = dsExpr syms e
+dsExpr syms (EDo mn (SBind i e : ss)) =
+  dsExpr syms $ EApp (EApp (EVar (mqual mn ">>=")) e) (ELam [i] (EDo mn ss))
+dsExpr syms (EDo mn (SThen   e : ss)) =
+  dsExpr syms $ EApp (EApp (EVar (mqual mn ">>")) e) (EDo mn ss)
+dsExpr syms (EDo mn (SLet ds : ss)) =
+  dsExpr syms $ ELet ds (EDo mn ss)
+dsExpr _ (EPrim s) = Prim s
+dsExpr syms (ESectL e op) =
+  App (dsExpr syms (EVar op)) (dsExpr syms e)
+dsExpr syms (ESectR op e) =
+  App2 CC (dsExpr syms (EVar op)) (dsExpr syms e)
+dsExpr syms (EIf e1 e2 e3) =
+  App2 (dsExpr syms e1) (dsExpr syms e3) (dsExpr syms e2)
+dsExpr syms (ECompr e []) = dsExpr syms (EList [e])
+dsExpr syms (ECompr e (SBind i b : ss)) =
+  App2 (Var "Data.List.concatMap") (dsExpr syms (ELam [i] (ECompr e ss))) (dsExpr syms b)
+dsExpr syms (ECompr e (SThen c : ss)) =
+  dsExpr syms (EIf c (ECompr e ss) (EList []))
+dsExpr syms (ECompr e (SLet d : ss)) =
+  dsExpr syms (ELet d (ECompr e ss))
 
 mqual :: Maybe Ident -> Ident -> Ident
 mqual (Just qi) i = qual qi i
@@ -160,9 +167,6 @@ apps f = foldl App f
 dsData :: EDef -> [TypeDef]
 dsData (Data (tn, _) cs) = [TypeDef tn [(c, length ts) | (c, ts) <- cs ]]
 dsData _ = []
-
-extSyms :: SymTable -> [Ident] -> SymTable
-extSyms = foldr (\ x -> M.insert x [Var x])
 
 patVars :: EPat -> [Ident]
 patVars (PConstr _ is) = is
