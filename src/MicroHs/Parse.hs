f37b389f8098f3e3a0705f6799a268cb3f83ef03
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-07-05 13:22:26 +0200

    Make it compile with uhs

--- src/MicroHs/Parse.hs
+++ src/MicroHs/Parse.hs
@@ -1,6 +1,8 @@
-{-# OPTIONS_GHC -Wno-type-defaults -Wno-name-shadowing #-}
+{-# OPTIONS_GHC -Wno-type-defaults -Wno-name-shadowing -Wno-unused-do-bind #-}
 {-# LANGUAGE QualifiedDo #-}
 module MicroHs.Parse(
+  module MicroHs.Parse
+{-
   pTop,
   parseDie,
   Ident,
@@ -15,8 +17,9 @@ module MicroHs.Parse(
   EModule(..),
   ExportSpec(..),
   LHS,
+-}
   ) where
-import Prelude hiding (Monad(..), Applicative(..), MonadFail(..), Functor(..), (<$>))
+import Prelude --Xhiding (Monad(..), Applicative(..), MonadFail(..), Functor(..), (<$>))
 --import Control.Monad
 --import Control.Monad.State.Strict
 --import Control.Applicative --hiding (many, some)
@@ -24,7 +27,14 @@ import Data.Char
 import Data.List
 import Text.ParserComb as P
 --import Debug.Trace
+--Ximport Compat
 
+-----------------------------
+
+pair :: a -> b -> (a, b)
+pair x y = (x, y)
+
+-----------------------------
 
 type P a = Prsr [Int] a
 
@@ -37,10 +47,10 @@ data EDef
   | Fcn LHS Expr
   | Sign Ident Type
   | Import ImportSpec
-  deriving (Show)
+  --Xderiving (Show)
 
 data ImportSpec = ImportSpec Bool Ident (Maybe Ident)
-  deriving (Show)
+  --Xderiving (Show)
 
 data Expr
   = EVar Ident
@@ -59,24 +69,24 @@ data Expr
   | ESectR Ident Expr
   | EIf Expr Expr Expr
   | ECompr Expr [EStmt]
-  deriving (Show)
+  --Xderiving (Show)
 
 data EStmt = SBind Ident Expr | SThen Expr | SLet [EBind]
-  deriving (Show)
+  --Xderiving (Show)
 
 data EBind = BFcn LHS Expr | BPat EPat Expr
-  deriving (Show)
+  --Xderiving (Show)
 
 data EPat
   = PConstr Ident [Ident]
   | PTuple [Ident]
-  deriving (Show)
+  --Xderiving (Show)
 
 data EModule = EModule IdentModule [ExportSpec] [EDef]
-  deriving (Show)
+  --Xderiving (Show)
 
 data ExportSpec = ExpModule IdentModule
-  deriving (Show)
+  --Xderiving (Show)
 
 type LHS = (Ident, [Ident])
 type Constr = (Ident, [Type])
@@ -88,19 +98,20 @@ qual qi i = qi ++ "." ++ i
 skipWhite :: P a -> P a
 skipWhite p = p <* pWhite
 
-skipWhite' :: P a -> P a
-skipWhite' p = p <* emany (char ' ')
+skipWhiteW :: P a -> P a
+skipWhiteW p = p <* emany (char ' ')
 
 -- Skip white-space.
 -- If there is a newline, return the indentation of the last line.
 pIndent :: P (Maybe String)
 pIndent = P.do
-  s <- emany (satisfy "white-space" (`elem` " \n\r"))
-  let s' = takeWhile (== ' ') $ reverse s
-  if s == s' then
+  s <- emany (satisfy "white-space" (\ c -> elemBy eqChar c " \n\r"))
+  let
+    ss = takeWhile (eqChar ' ') $ reverse s
+  if eqString s ss then
     pure Nothing
    else
-    pure $ Just s'
+    pure $ Just ss
 
 pWhite :: P ()
 pWhite = P.do
@@ -128,65 +139,78 @@ esepBy1 p sep = (:) <$> p <*> emany (sep *> p)
 esepBy :: Prsr s a -> Prsr s sep -> Prsr s [a]
 esepBy p sep = esepBy1 p sep <|> pure []
 
-parseDie :: (Show a) => P a -> FilePath -> String -> a
+--XparseDie :: (Show a) => P a -> FilePath -> String -> a
 parseDie p fn file =
   case runPrsr [] p fn (removeComments file) of
     Left err -> error err
-    Right [(x, _)] -> x
-    Right as -> error $ "Ambiguous:\n" ++ unlines (map (show . fst) as)
+    Right as ->
+-- XXX parsing foo $ do ... is ambiguous
+      if length as == 1 then
+        fst (head as)
+      else
+        error $ "Ambiguous:"
+--                 ++ unlines (map (show . fst) as)
 
 -- Remove comments first instead of having them in the parser.
 removeComments :: String -> String
-removeComments =
-  let
-    remCom acs =
-      case stripPrefix "--" acs of
-        Just cs -> skipToNL cs
+removeComments = remCom
+
+remCom :: [Char] -> [Char]
+remCom acs =
+  case stripPrefixBy eqChar "--" acs of
+    Just cs -> skipToNL cs
+    Nothing ->
+      case stripPrefixBy eqChar "{-" acs of
+        Just cs -> skipBlock 1 cs
         Nothing ->
-          case stripPrefix "{-" acs of
-            Just cs -> skipBlock 1 cs
-            Nothing ->
-              case acs of
-                [] -> ""
-                c:cs ->
-                  if c == '"' then '"' : str cs
-                  else if c == '\'' then '\'' : chr cs
-                  else c : remCom cs
-    str acs =
-      case acs of
-        [] -> ""
-        c:cs ->
-          if c == '"' then '"' : remCom cs
-          else if c == '\\' then '\\':head cs:str (tail cs)
-          else c : str cs
-    chr acs =
-      case acs of
-        [] -> ""
-        c:cs ->
-          if c == '\'' then '\'' : remCom cs
-          else if c == '\\' then '\\':head cs:chr (tail cs)
-          else c : chr cs
-    skipToNL acs =
-      case acs of
-        [] -> ""
-        c:cs ->
-          if c == '\n' then '\n' : remCom cs
-          else skipToNL cs
-    skipBlock n acs =
-      if n == 0 then
-        remCom acs
-      else
-        case stripPrefix "{-" acs of
-          Just cs -> skipBlock (n+1) cs
+          case acs of
+            [] -> ""
+            c:cs ->
+              if eqChar c '"' then '"' : getStr cs
+              else if eqChar c '\'' then '\'' : getChr cs
+              else c : remCom cs
+
+getStr :: [Char] -> [Char]
+getStr acs =
+  case acs of
+    [] -> ""
+    c:cs ->
+      if eqChar c '"' then '"' : remCom cs
+      else if eqChar c '\\' then '\\':head cs:getStr (tail cs)
+      else c : getStr cs
+
+getChr :: [Char] -> [Char]
+getChr acs =
+  case acs of
+    [] -> ""
+    c:cs ->
+      if eqChar c '\'' then '\'' : remCom cs
+      else if eqChar c '\\' then '\\':head cs:getChr (tail cs)
+      else c : getChr cs
+
+skipToNL :: [Char] -> [Char]
+skipToNL acs =
+  case acs of
+    [] -> ""
+    c:cs ->
+      if eqChar c '\n' then '\n' : remCom cs
+      else skipToNL cs
+
+skipBlock :: Int -> [Char] -> [Char]
+skipBlock n acs =
+  if n == 0 then
+    remCom acs
+  else
+    case stripPrefixBy eqChar "{-" acs of
+      Just cs -> skipBlock (n+1) cs
+      Nothing ->
+        case stripPrefixBy eqChar "-}" acs of
+          Just cs -> skipBlock (n-1) cs
           Nothing ->
-            case stripPrefix "-}" acs of
-              Just cs -> skipBlock (n-1) cs
-              Nothing ->
-                case acs of
-                  [] -> ""
-                  c:cs -> if c == '\n' then '\n' : skipBlock n cs
-                          else skipBlock n cs
-  in  remCom
+            case acs of
+              [] -> ""
+              c:cs -> if eqChar c '\n' then '\n' : skipBlock n cs
+                      else skipBlock n cs
 {-
         remCom ('-':'-':cs) = skipToNL cs
         remCom ('{':'-':cs) = skipBlock 1 cs
@@ -214,52 +238,40 @@ removeComments =
         skipBlock _ "" = ""
 -}
 
--------
-{-
-isLower :: Char -> Bool
-isLower c = 'a' <= c && c <= 'z'
-
-isUpper :: Char -> Bool
-isUpper c = 'A' <= c && c <= 'Z'
-
-isLetter :: Char -> Bool
-isLetter c = isLower c || isUpper c
-
-isDigit :: Char -> Bool
-isDigit c = '0' <= c && c <= '9'
--}
--------
-
 pTop :: P EModule
 pTop = skipWhite (pure ()) *> pModule <* eof
 
 pModule :: P EModule
 pModule = EModule <$> (pKeyword "module" *> pUIdent) <*>
                       (pSym '(' *> esepBy pExportSpec (pSym ',') <* pSym ')') <*>
-                      (pKeyword' "where" *> pBlock pDef)
+                      (pKeywordW "where" *> pBlock pDef)
 
 pExportSpec :: P ExportSpec
 pExportSpec = ExpModule <$> (pKeyword "module" *> pUIdent)
 
 pKeyword :: String -> P ()
-pKeyword kw = (skipWhite $ P.do
-  s <- pWord
-  guard (kw == s)
-  pure ()
-  ) <?> kw
+pKeyword kw = skipWhite (
+  P.do
+    s <- pWord
+    guard (eqString kw s)
+    pure ()
+  )
 
-pKeyword' :: String -> P ()
-pKeyword' kw = (skipWhite' $ P.do
-  s <- pWord
-  guard (kw == s)
-  pure ()
-  ) <?> kw
+pKeywordW :: String -> P ()
+pKeywordW kw = skipWhiteW (
+  P.do
+    s <- pWord
+    guard (eqString kw s)
+    pure ()
+  )
 
 pLIdentA :: P String
-pLIdentA = skipWhite $ P.do
-  s <- pQIdent
-  guard $ isLower $ head s
-  pure s
+pLIdentA = skipWhite (
+  P.do
+    s <- pQIdent
+    guard $ isLower $ head s
+    pure s
+  )
 
 pLIdent :: P String
 pLIdent = pLIdentA <|> (pSym '(' *> pOperL <* pSym ')')
@@ -268,10 +280,12 @@ pLIdent_ :: P String
 pLIdent_ = pLIdent <|> skipWhite (string "_")
 
 pUIdentA :: P String
-pUIdentA = skipWhite $ P.do
-  s <- pQIdent
-  guard $ isUpper $ head s
-  pure s
+pUIdentA = skipWhite (
+  P.do
+    s <- pQIdent
+    guard $ isUpper $ head s
+    pure s
+  )
 
 pUIdent :: P String
 pUIdent = pUIdentA <|> (pSym '(' *> pOperU <* pSym ')')
@@ -284,94 +298,104 @@ pWord :: P String
 pWord = (:) <$> satisfy "letter" isLetter <*>
                 (emany $ satisfy "letter, digit" $ \ c ->
                     isLetter c || isDigit c ||
-                    c == '_' || c == '\'')
+                    eqChar c '_' || eqChar c '\'')
 
 pIdent :: P String
 pIdent = P.do
   s <- pWord
-  guard (s `notElem` keywords)
+  guard (not (elemBy eqString s keywords))
   pure s
 
 pQIdent :: P String
 pQIdent = intercalate "." <$> esepBy1 pIdent (char '.')
 
 pInt :: P Int
-pInt = (read <$> (skipWhite $ esome $ satisfy "digit" isDigit)) <?> "int"
+pInt = readInt <$> (skipWhite $ esome $ satisfy "digit" isDigit)
 
 pChar :: P Char
-pChar = skipWhite (char '\'' *> pc <* char '\'')
-  where pc = P.do
-          c <- satisfy "char" (/= '\'')
-          if c == '\\' then
-            decodeChar <$> satisfy "char" (const True)
-           else
-            pure c
+pChar =
+  let
+    pc =
+      P.do
+        c <- satisfy "char" (neChar '\'')
+        if eqChar c '\\' then
+          decodeChar <$> satisfy "char" (const True)
+         else
+          pure c
+  in  skipWhite (char '\'' *> pc <* char '\'')
 
 pString :: P String
-pString = skipWhite (char '"' *> emany pc <* char '"')
-  where pc = P.do
-          c <- satisfy "char" (/= '"')
-          guard (c /= '\n')
-          if c == '\\' then
-            decodeChar <$> satisfy "char" (const True)
-           else
-            pure c
+pString =
+  let
+    pc =
+      P.do
+        c <- satisfy "char" (neChar '"')
+        guard (neChar c '\n')
+        if eqChar c '\\' then
+          decodeChar <$> satisfy "char" (const True)
+         else
+          pure c
+  in  skipWhite (char '"' *> emany pc <* char '"')
 
 decodeChar :: Char -> Char
-decodeChar 'n' = '\n'
-decodeChar c = c
+decodeChar c =
+  if eqChar c 'n' then
+    '\n'
+  else
+    c
 
 pSymbol :: String -> P ()
-pSymbol s = (P.do
-  s' <- pOper'
-  guard (s == s')
+pSymbol s = P.do
+  ss <- pOperW
+  guard (eqString s ss)
   pure ()
-  ) <?> s
 
-pOper' :: P String
-pOper' = skipWhite $ esome $ satisfy "symbol" (`elem` "@\\=+-:<>.!#$%^&*/|~?")
+pOperW :: P String
+pOperW = skipWhite $ esome $ satisfy "symbol" (\x -> elemBy eqChar x "@\\=+-:<>.!#$%^&*/|~?")
 
 pOper :: P String
 pOper = P.do
-  s <- pOper'
-  guard $ s `notElem` ["=", "|", "::", "<-", "@"]
+  s <- pOperW
+  guard $ not $ elemBy eqString s ["=", "|", "::", "<-", "@"]
   pure s
 
 pOperL :: P String
 pOperL = P.do
   s <- pOper
-  guard $ head s /= ':'
+  guard $ neChar (head s) ':'
   pure s
 
 pOperU :: P String
 pOperU = P.do
   s <- pOper
-  guard $ head s == ':'
+  guard $ eqChar (head s) ':'
   pure s
 
 pOpers :: [String] -> P String
 pOpers ops = P.do
   op <- pOper
-  guard (op `elem` ops)
+  guard (elemBy eqString op ops)
   pure op
 
 pSym :: Char -> P ()
 pSym c = () <$ (skipWhite $ char c)
 
 pLHS :: P Ident -> P LHS
-pLHS pId = (,) <$> pId <*> many pLIdent_
+pLHS pId = pair <$> pId <*> many pLIdent_
 
 pDef :: P EDef
 pDef =
-      Data   <$> (pKeyword "data" *> pLHS pUIdent <* pSymbol "=") <*> esepBy1 ((,) <$> pUIdent <*> many pAType) (pSymbol "|")
+      Data   <$> (pKeyword "data" *> pLHS pUIdent <* pSymbol "=") <*> esepBy1 (pair <$> pUIdent <*> many pAType) (pSymbol "|")
   <|> Type   <$> (pKeyword "type" *> pLHS pUIdent <* pSymbol "=") <*> pType
   <|> Fcn    <$> (pLHS pLIdent_ <* pSymbol "=") <*> pExpr
   <|> Sign   <$> (pLIdent <* pSymbol "::") <*> pType
   <|> Import <$> (pKeyword "import" *> pImportSpec)
 
 pImportSpec :: P ImportSpec
-pImportSpec = ImportSpec <$> pQua <*> pUIdent <*> optional (pKeyword "as" *> pUIdent)
-  where pQua = (True <$ pKeyword "qualified") <|< pure False
+pImportSpec =
+  let
+    pQua = (True <$ pKeyword "qualified") <|< pure False
+  in  ImportSpec <$> pQua <*> pUIdent <*> optional (pKeyword "as" *> pUIdent)
 
 pAType :: P Type
 pAType = pAExpr
@@ -403,8 +427,10 @@ pLam :: P Expr
 pLam = ELam <$> (pSymbol "\\" *> esome pLIdent_) <*> (pSymbol "->" *> pExpr)
 
 pCase :: P Expr
-pCase = ECase <$> (pKeyword "case" *> pExpr) <*> (pKeyword' "of" *> pBlock pArm)
-  where pArm = (,) <$> (pPat <* pSymbol "->") <*> pExpr
+pCase =
+  let
+    pArm = pair <$> (pPat <* pSymbol "->") <*> pExpr
+  in  ECase <$> (pKeyword "case" *> pExpr) <*> (pKeywordW "of" *> pBlock pArm)
 
 pPat :: P EPat
 pPat =
@@ -415,25 +441,26 @@ pPat =
   <|> ((\ x s y -> PConstr s [x,y]) <$> pLIdent_ <*> pOperU <*> pLIdent_)
 
 pLet :: P Expr
-pLet = ELet <$> (pKeyword' "let" *> pBlock pBind) <*> (pKeyword "in" *> pExpr)
+pLet = ELet <$> (pKeywordW "let" *> pBlock pBind) <*> (pKeyword "in" *> pExpr)
 
 pExprOp :: P Expr
-pExprOp = p0
-  where
-    p0 = pRightAssoc (pOpers ["$", "->"]) p1   -- XXX where should -> be?
-    p1 = pLeftAssoc  (pOpers [">>=", ">>"]) p2
-    p2 = pRightAssoc (pOpers ["||"]) p3
-    p3 = pRightAssoc (pOpers ["&&"]) $
-         pLeftAssoc  (pOpers ["<|>","<|<"]) p4
-    p4 = pNonAssoc   (pOpers ["==", "/=", "<", "<=", ">", ">="]) $
-         pLeftAssoc  (pOpers ["<*>", "<*", "*>", "<$>", "<$"])   p5
-    p5 = pRightAssoc (pOpers [":", "++"]) p6
-    p6 = pLeftAssoc  (pOpers ["+", "-"]) p7
-    p7 = pLeftAssoc  (pOpers ["*", "quot", "rem"]) p8
-    p8 = p9
+pExprOp =
+  let
+    p10 = pExprArg
     p9 = pRightAssoc (pOpers ["."]) $
          pLeftAssoc  (pOpers ["?"]) p10
-    p10 = pExprArg
+    p8 = p9
+    p7 = pLeftAssoc  (pOpers ["*", "quot", "rem"]) p8
+    p6 = pLeftAssoc  (pOpers ["+", "-"]) p7
+    p5 = pRightAssoc (pOpers [":", "++"]) p6
+    p4 = pNonAssoc   (pOpers ["==", "/=", "<", "<=", ">", ">="]) $
+         pLeftAssoc  (pOpers ["<*>", "<*", "*>", "<$>", "<$"])   p5
+    p3 = pRightAssoc (pOpers ["&&"]) $
+         pLeftAssoc  (pOpers ["<|>","<|<"]) p4
+    p2 = pRightAssoc (pOpers ["||"]) p3
+    p1 = pLeftAssoc  (pOpers [">>=", ">>"]) p2
+    p0 = pRightAssoc (pOpers ["$", "->"]) p1   -- XXX where should -> be?
+  in  p0
 
 appOp :: String -> Expr -> Expr -> Expr
 appOp op e1 e2 = EApp (EApp (EVar op) e1) e2
@@ -441,7 +468,9 @@ appOp op e1 e2 = EApp (EApp (EVar op) e1) e2
 pRightAssoc :: P String -> P Expr -> P Expr
 pRightAssoc pOp p = P.do
   e1 <- p
-  let rest = P.do
+  let
+    rest =
+      P.do
         op <- pOp
         e2 <- pRightAssoc pOp p
         pure $ appOp op e1 e2
@@ -450,7 +479,9 @@ pRightAssoc pOp p = P.do
 pNonAssoc :: P String -> P Expr -> P Expr
 pNonAssoc pOp p = P.do
   e1 <- p
-  let rest = P.do
+  let
+    rest =
+      P.do
         op <- pOp
         e2 <- p
         pure $ appOp op e1 e2
@@ -459,8 +490,8 @@ pNonAssoc pOp p = P.do
 pLeftAssoc :: P String -> P Expr -> P Expr
 pLeftAssoc pOp p = P.do
   e1 <- p
-  es <- emany ((,) <$> pOp <*> p)
-  pure $ foldl (\ x (op, y) -> appOp op x y) e1 es
+  es <- emany (pair <$> pOp <*> p)
+  pure $ foldl (\ x opy -> appOp (fst opy) x (snd opy)) e1 es
 
 pExprArg :: P Expr
 pExprArg = pExprApp <|> pLam <|> pCase <|> pLet <|> pIf
@@ -469,7 +500,7 @@ pExpr :: P Expr
 pExpr = pExprOp <|> pDo
 
 pDo :: P Expr
-pDo = EDo <$> ((Just <$> pQualDo) <|< (Nothing <$ pKeyword' "do")) <*> pBlock pStmt
+pDo = EDo <$> ((Just <$> pQualDo) <|< (Nothing <$ pKeywordW "do")) <*> pBlock pStmt
 
 pIf :: P Expr
 pIf = EIf <$> (pKeyword "if" *> pExpr) <*> (pKeyword "then" *> pExpr) <*> (pKeyword "else" *> pExpr)
@@ -477,7 +508,7 @@ pIf = EIf <$> (pKeyword "if" *> pExpr) <*> (pKeyword "then" *> pExpr) <*> (pKeyw
 pStmt :: P EStmt
 pStmt =
       (SBind <$> (pLIdent <* pSymbol "<-") <*> pExpr)
-  <|> (SLet  <$> (pKeyword' "let" *> pBlock pBind))
+  <|> (SLet  <$> (pKeywordW "let" *> pBlock pBind))
   <|> (SThen <$> pExpr)
 
 pBind :: P EBind
@@ -488,29 +519,35 @@ pBind =
 pQualDo :: P String
 pQualDo = P.do
   s <- pUIdent
-  _ <- char '.'
-  pKeyword' "do"
+  char '.'
+  pKeywordW "do"
   pure s
 
 pLCurl :: P ()
-pLCurl = pSym '{' <|< softLC
-  where softLC = P.do
-          msp <- pIndent
-          case msp of
-            Nothing -> fail "\\n"
-            Just sp -> P.do
---              traceM ("push " ++ show (length sp))
-              modify $ \ st -> length sp : st
+pLCurl =
+  let
+    softLC =
+      P.do
+        msp <- pIndent
+        case msp of
+          Nothing -> fail "\\n"
+          Just sp -> P.do
+--            traceM ("push " ++ show (length sp))
+            modify $ \ st -> length sp : st
+  in  pSym '{' <|< softLC
 
 pRCurl :: P ()
-pRCurl = pSym '}' <|> softRC
-  where softRC = P.do
-          is <- get
-          if null is then
-            fail "}"
-           else P.do
-            put (tail is)
-            eof
+pRCurl =
+  let
+    softRC =
+      P.do
+        is <- get
+        if null is then
+          fail "}"
+         else P.do
+          put (tail is)
+          eof
+  in  pSym '}' <|> softRC
 
 pBlock :: P a -> P [a]
 pBlock p = P.do
