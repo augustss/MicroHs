acf40ed435abf7080723bc4f98ad764ef221311b
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-07-19 13:11:39 +0200

    Remove junk

--- A.hs
+++
@@ -1,9 +0,0 @@
-module A(module A) where
-import Prelude
-
-main = do
-  putStrLn $ showList showInt $ map (+ 1) [2,3]
-  putStrLn $ showList showInt $ map (1 +) [2,3]
-  putStrLn $ showList showInt $ map (- 1) [2,3]
-  putStrLn $ showList showInt $ map (1 -) [2,3]
-  
