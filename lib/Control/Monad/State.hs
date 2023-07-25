17b450fc8ca6bb36471dcf58801217f05c6413a3
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-07-25 14:58:34 +0200

    Add more lib modules.

---
+++ lib/Control/Monad/State.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE QualifiedDo #-}
+module Control.Monad.State(module Control.Monad.State) where
+import Prelude
+
+data State s a = S (s -> (a, s))
+
+runState :: State s a -> (s -> (a,s))
+runState sa =
+  case sa of
+    S x -> x
+
+(>>=) :: State s a -> (a -> State s b) -> State s b
+(>>=) m k = S $ \ s ->
+  case runState m s of
+    (a, ss) -> runState (k a) ss
+
+(>>) :: State s a -> State s b -> State s b
+(>>) m k = S $ \ s ->
+  case runState m s of
+    (_, ss) -> runState k ss
+
+return :: a -> State s a
+return a = S $ \ s -> (a, s)
+
+fmap :: (a -> b) -> State s a -> State s b
+fmap f sa = S $ \ s ->
+  case runState sa s of
+    (a, ss) -> (f a, ss)
+
+modify :: (s -> s) -> State s ()
+modify f = S $ \ s -> ((), f s)
+
+put :: s -> State s ()
+put s = S $ \ _ -> ((), s)
+
+get :: State s s
+get = S $ \ s -> (s, s)
+
+gets :: (s -> a) State s a
+gets f = S $ \ s -> (f s, s)
+
+mapM :: (a -> State s b) -> [a] -> State s [b]
+mapM f =
+  let
+    rec arg =
+      case arg of
+        [] -> Control.Monad.State.return []
+        a : as -> Control.Monad.State.do
+          b <- f a
+          bs <- rec as
+          Control.Monad.State.return (b : bs)
+  in rec
---
+++ lib/Data/IntMap.hs
@@ -0,0 +1,77 @@
+-- Copyright 2023 Lennart Augustsson
+-- See LICENSE file for full license.
+module Data.IntMap(module Data.IntMap) where
+import Prelude hiding(lookup)
+
+data IntMap a
+  = Empty
+  | Leaf Int a
+  | Node (IntMap a) (IntMap a) (IntMap a) (IntMap a)
+  deriving (Show)
+
+-- This works for y>0
+divModX :: Int -> Int -> (Int, Int)
+divModX x y =
+  let
+    q = quot x y
+    r = rem x y
+  in
+    if x >= 0 || r == 0 then
+      (q, r)
+    else
+      (q-1, r+y)
+
+empty :: forall a . IntMap a
+empty = Empty
+
+lookup :: forall a . Int -> IntMap a -> Maybe a
+lookup k am =
+  case am of
+    Empty -> Nothing
+    Leaf i a -> if k == i then Just a else Nothing
+    Node m0 m1 m2 m3 ->
+      let
+        (d, m) = divModX k 4
+      in      if m == 0 then lookup d m0
+         else if m == 1 then lookup d m1
+         else if m == 2 then lookup d m2
+         else                lookup d m3
+
+insert :: forall a . Int -> a -> IntMap a -> IntMap a
+insert ak a =
+  let
+    ins k am =
+      case am of
+        Empty -> Leaf k a
+        Leaf i b ->
+          if k == i then
+            Leaf k a
+          else
+            ins k $ insert i b $ Node Empty Empty Empty Empty
+        Node m0 m1 m2 m3 ->
+          let
+            (d, m) = divModX k 4
+          in      if m == 0 then Node (ins d m0) m1 m2 m3
+             else if m == 1 then Node m0 (ins d m1) m2 m3
+             else if m == 2 then Node m0 m1 (ins d m2) m3
+             else                Node m0 m1 m2 (ins d m3)
+  in ins ak
+
+fromList :: forall a . [(Int, a)] -> IntMap a
+fromList = foldr (uncurry insert) empty
+
+toList :: forall a . IntMap a -> [(Int, a)]
+toList am =
+  let
+    f o ka =
+      case ka of
+        (k, a) -> (k*4 + o, a)
+  in
+    case am of
+      Empty -> []
+      Leaf l a -> [(l, a)]
+      Node m0 m1 m2 m3 ->
+        map (f 0) (toList m0) ++
+        map (f 1) (toList m1) ++
+        map (f 2) (toList m2) ++
+        map (f 3) (toList m3)
