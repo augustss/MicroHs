3617e0f7af66ff17acfcb539b3862a5f60010a5b
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-09-18 23:50:45 +0200

    Make length tail recursive.

--- lib/Data/List.hs
+++ lib/Data/List.hs
@@ -100,8 +100,14 @@ drop n arg =
       _ : xs -> drop (n - 1) xs
 
 length :: forall a . [a] -> P.Int
-length [] = 0
-length (_:xs) = 1 + length xs
+length =
+  -- Make it tail recursive and strict
+  let
+    rec r [] = r
+    rec r (_:xs) =
+          let r' = r + 1
+          in  r' `primSeq` rec r' xs
+  in rec 0
 
 zip :: forall a b . [a] -> [b] -> [(a, b)]
 zip = zipWith (\ x y -> (x, y))
