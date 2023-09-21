47235546ec225eb4885bb9793b84d2f2df73dc45
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-09-22 00:05:56 +0200

    Improve statistics tool.

--- Tools/Stats.hs
+++ Tools/Stats.hs
@@ -10,7 +10,11 @@ main = do
       loop m "" = m
       loop m cs@(_:ds) | Just (x,y) <- getComb cs = loop (M.insertWith (+) x 1 m) y
                        | otherwise = loop m ds
-      getComb ('(':'$':cs) = Just ('$':xs, tail ys) where (xs, ys) = span (/= ')') cs
+      getComb ('(':'$':cs) = --Just ('$':xs, tail ys) where (xs, ys) = span (/= ')') cs
+        let (k1, r) = span (/= ' ') cs
+        in case r of
+             ' ':'$':s -> let (k2, t) = span (/= ')') s in Just ("($" ++ k1 ++ " $" ++ k2 ++ ")", tail t)
+             _ -> Nothing
       getComb _ = Nothing
       srt = sortBy (compare `on` snd) $ M.toList res
   mapM print srt
