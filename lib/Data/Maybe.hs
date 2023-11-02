7ef353722df91c6bd4dd0a3ad4d7db0f0bb77f72
Lennart Augustsson <lennart@augustsson.net>
2023-11-02 14:50:34 +0100

    Monad Maybe

--- lib/Data/Maybe.hs
+++ lib/Data/Maybe.hs
@@ -2,16 +2,18 @@
 -- See LICENSE file for full license.
 module Data.Maybe(module Data.Maybe, module Data.Maybe_Type) where
 import Primitives
+import Control.Applicative
+import Control.Monad
 import Data.Bool
 import Data.Char
 import Data.Eq
 import Data.Function
+import Data.Functor
 import Data.Int
 import Data.List
 import Data.Maybe_Type
 import Data.Ord
 import Text.Show
-
 
 instance forall a . Eq a => Eq (Maybe a) where
   Nothing == Nothing  =  True
@@ -22,7 +24,22 @@ instance forall a . (Show a) => Show (Maybe a) where
   showsPrec _ Nothing  = showString "Nothing"
   showsPrec p (Just a) = showParen (p >= 11) (showString "Just " . showsPrec 11 a)
 
--- XXX instance Monad Maybe
+instance Functor Maybe where
+  fmap _ Nothing  = Nothing
+  fmap f (Just a) = Just (f a)
+
+instance Applicative Maybe where
+  pure = Just
+  Just f <*> Just a = Just (f a)
+  _      <*> _      = Nothing
+
+instance Monad Maybe where
+  return = pure
+  Nothing >>= _ = Nothing
+  Just a  >>= f = f a
+
+instance MonadFail Maybe where
+  fail _ = Nothing
 
 maybe :: forall a r . r -> (a -> r) -> Maybe a -> r
 maybe r _ Nothing = r
