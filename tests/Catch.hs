1b2656b4b9409ee1d27707b816cfe5578aa3ef16
Lennart Augustsson <lennart@augustsson.net>
2023-11-08 14:40:10 +0000

    More tests.

--- tests/Catch.hs
+++ tests/Catch.hs
@@ -2,9 +2,21 @@ module Catch(main) where
 import Prelude
 import Control.Exception
 
+f :: [Int] -> Int
+f (_:_) = 99
+
+class C a where
+  m :: a -> Int
+
+instance C ()
+
 main :: IO ()
 main = do
   x <- catch (return ("o" ++ "k")) (\ _ -> return "what?")
   putStrLn $ show x
-  y <- catch (do { error "bang!"; return "huh?" }) (\ (Exn s) -> return s)
+  y <- catch (do { error "bang!"; return "yyy" }) (\ (Exn s) -> return s)
   putStrLn $ show y
+  z <- catch (do { print (f []); return "zzz" })  (\ (Exn s) -> return s)
+  putStrLn $ show z
+  w <- catch (do { print (m ()); return "www" })  (\ (Exn s) -> return s)
+  putStrLn $ show w
