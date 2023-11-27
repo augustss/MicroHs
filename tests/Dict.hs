645abd7ece2fe3132c36078399417f625f17e1f2
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-11-27 20:45:37 +0100

    Another test.

--- tests/Dict.hs
+++ tests/Dict.hs
@@ -8,10 +8,17 @@ fac n = if n == 0 then 1 else n * fac(n - 1)
 facD :: forall a . Dict (Num a, Eq a) -> a -> a
 facD Dict = fac
 
+facDO :: forall a . Dict (Ord a, Num a) -> a -> a
+facDO Dict = fac
+
 dictInt :: Dict (Num Int, Eq Int)
 dictInt = Dict
+
+dictIntO :: Dict (Ord Int, Num Int)
+dictIntO = Dict
 
 main :: IO ()
 main = do
   print $ facD dictInt 10
   print $ withDict dictInt (fac (11::Int))
+  print $ facDO dictIntO 12
