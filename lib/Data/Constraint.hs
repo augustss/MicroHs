513fd84d2f280fe0ddf4bf73705b56efc7ad0a3d
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-11-27 13:19:35 +0100

    Add missing import.

--- lib/Data/Constraint.hs
+++ lib/Data/Constraint.hs
@@ -1,6 +1,7 @@
 -- Copyright 2023 Lennart Augustsson
 -- See LICENSE file for full license.
 module Data.Constraint(module Data.Constraint) where
+import Primitives  -- for (->)
 -- A very, very minimal version of the constraints package
 
 data Dict (c :: Constraint) = c => Dict
