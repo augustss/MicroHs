bfea4e5ee9483831dc316f728acc702b39c593ea
Lennart Augustsson <lennart.augustsson@epicgames.com>
2023-12-17 20:54:54 +0100

    Start by printing system info.

---
+++ tests/Info.hs
@@ -0,0 +1,23 @@
+module Info(main) where
+import Prelude
+import Foreign.Storable
+import Foreign.Marshal.Utils
+import Foreign.Ptr
+import Data.Word
+import Data.Word8
+
+main :: IO ()
+main = do
+  putStrLn $ "Running on " ++ if _isWindows then "Windows" else "Unix"
+  putStr $ show _wordSize ++ " bit words, "
+
+  let
+    w :: Word
+    w = if _wordSize == 32 then 0x01000002 else 0x0100000000000002
+  p <- new w
+  b <- peek (castPtr p :: Ptr Word8)
+  putStrLn $
+    case b of
+      1 -> "big endian"
+      2 -> "little endian"
+      _ -> "Mystery Endian"
