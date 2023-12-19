fe359d7d4a3701fdc9d18a5dcf95ade21690a731
Lennart Augustsson <lennart@augustsson.net>
2023-12-19 20:25:30 +0100

    Make md5 checksums smaller

--- lib/System/IO/MD5.hs
+++ lib/System/IO/MD5.hs
@@ -1,16 +1,18 @@
 -- Copyright 2023 Lennart Augustsson
 -- See LICENSE file for full license.
-module System.IO.MD5(MD5CheckSum, md5File, md5Handle) where
+module System.IO.MD5(MD5CheckSum, md5File, md5Handle, md5String) where
 import Primitives(primUnsafeCoerce)
 import Prelude
 import Data.Word
 import Foreign.C.String
 import Foreign.Marshal.Alloc
+import Foreign.Marshal.Array
 import Foreign.Ptr
 
-foreign import ccall "md5File" c_md5File :: Handle -> CString -> IO ()
+foreign import ccall "md5File"   c_md5File   :: Handle  -> Ptr Word -> IO ()
+foreign import ccall "md5String" c_md5String :: CString -> Ptr Word -> IO ()
 
-newtype MD5CheckSum = MD5 [Word]  -- actually returns 16 bytes
+newtype MD5CheckSum = MD5 [Word]  -- either 2*64 bits or 4*32 bits
 
 instance Eq MD5CheckSum where
   MD5 a == MD5 b  =  a == b
@@ -19,18 +21,24 @@ instance Show MD5CheckSum where
   show (MD5 ws) = "MD5" ++ show ws
 
 md5Len :: Int
-md5Len = 16   -- The MD5 checksum is 16 bytes, use one byte / word
+md5Len = 16   -- The MD5 checksum is 16 bytes
 
-md5Handle :: Handle -> IO MD5CheckSum
-md5Handle h = do
-  buf <- mallocBytes md5Len
-  c_md5File h buf
-  md5 <- peekCAStringLen (buf, md5Len)
-  let
-    wmd5 :: [Word]
-    wmd5 = primUnsafeCoerce md5
+md5WLen :: Int  -- length in words
+md5WLen = (md5Len * 8) `quot` _wordSize
+
+chksum :: (Ptr Word -> IO ()) -> IO MD5CheckSum
+chksum fn = do
+  buf <- mallocArray md5WLen
+  fn buf
+  wmd5 <- peekArray md5WLen buf
   free buf
   return (MD5 wmd5)
+
+md5String :: String -> IO MD5CheckSum
+md5String s = withCAString s $ chksum . c_md5String
+
+md5Handle :: Handle -> IO MD5CheckSum
+md5Handle h = chksum $ c_md5File h
 
 md5File :: FilePath -> IO (Maybe MD5CheckSum)
 md5File fn = do
