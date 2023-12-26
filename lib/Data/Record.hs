b2de98441112c9eac3de6d7d43537fed2dc66855
Lennart Augustsson <lennart@augustsson.net>
2023-12-26 16:29:36 -0700

    Make virtual fields for tuples.

--- lib/Data/Record.hs
+++ lib/Data/Record.hs
@@ -2,14 +2,13 @@ module Data.Record(module Data.Record) where
 import Primitives
 import Data.Function
 import Data.Proxy
-import Data.Tuple
 
 type  HasField :: forall (k::Kind) . k -> Type -> Type -> Constraint
 class HasField x r a | x r -> a where
   hasField :: Proxy x -> r -> (a, a -> r)
 
 getField :: forall x r a . HasField x r a => Proxy x -> r -> a
-getField p = fst . hasField p
+getField p r = case hasField p r of { (g,_) -> g }
 
-setField :: forall x r a . HasField x r a => Proxy x -> r -> a -> r
-setField p = snd . hasField p
+setField :: forall x r a . HasField x r a => Proxy x -> r -> (a -> r)
+setField p r = case hasField p r of { (_,s) -> s }
