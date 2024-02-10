3052533937e495329ba39ab8fba055956d152d17
Lennart Augustsson <lennart.augustsson@epicgames.com>
2024-02-10 17:57:22 +0100

    Add missing files

---
+++ tests/ParseInd.hs
@@ -0,0 +1,19 @@
+module ParseInd(main) where
+import Prelude
+
+-- The parse error generateing a '}'
+
+main :: IO ()
+main = do
+  print (foo 1)
+  print (bar True)
+  baz 1
+
+foo :: Int -> Int
+foo a = let x = a+1 in x
+
+bar :: Bool -> Bool
+bar x = not (case x of True -> False; False -> True)
+
+baz :: Int -> IO ()
+baz i = (do print i) >> (do print (i+1))
