8693a0697d478761f316806f7f2b9e3cabf8f327
Lennart Augustsson <lennart.augustsson@epicgames.com>
2024-02-15 15:03:32 +0100

    Remove some junk

--- Example.hs
+++ Example.hs
@@ -1,5 +1,4 @@
 module Example(fac, main) where
-import Prelude
 
 fac :: Int -> Int
 fac 0 = 1
@@ -7,8 +6,6 @@ fac n = n * fac(n - 1)
 
 main :: IO ()
 main = do
-  --putStrLn $ "word size=" ++ show _wordSize ++ ", os=" ++ if _isWindows then "Windows" else "Unix-like"
-  let
-    rs = map fac [1,2,3,10]
+  let rs = map fac [1,2,3,10]
   putStrLn "Some factorials"
   print rs
