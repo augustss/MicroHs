c1310d046961f79579b8304f1da251df5df5d100
Lennart Augustsson <lennart.augustsson@epicgames.com>
2024-02-25 18:57:11 +0100

    Change tests for 32 bit float

--- tests/FArith.hs
+++ tests/FArith.hs
@@ -15,6 +15,6 @@ main = do
   putStrLn $ show [ op x y | x <- list1, y <- list1, op <- [(+), (-), (*)] ]
   putStrLn $ show [ op x y | x <- list1, y <- list1, op <- [(==), (/=), (<), (<=), (>), (>=)] ]
   putStrLn $ show [ x / y  | x <- list1, y <- [1.0::Double, -16.0::Double, 0.0625::Double]]
-  putStrLn $ show $ readDouble "1.565"
+  putStrLn $ show $ readDouble "1.625"
   putStrLn $ show $ 1.0 + readDouble "2.5"
-  putStrLn $ show $ map readDouble ["1.5e32", "1.25e-33"]
+  putStrLn $ show $ map readDouble ["1.5e10", "1.25e-11"]
