bcb571d56500ce09f9994d7ae777e830c9a6e629
Lennart Augustsson <lennart@augustsson.net>
2024-04-06 21:38:08 -0600

    Fix bug in test

--- tests/ForeignPtr.hs
+++ tests/ForeignPtr.hs
@@ -9,18 +9,21 @@ gc = primitive "IO.gc"
 add :: Ptr a -> Int -> Ptr a
 add = plusPtr
 
+sInt :: Int
+sInt = sizeOf (0::Int)
+
 main :: IO ()
 main = do
   fp <- mallocForeignPtrArray 2
   withForeignPtr fp $ \ p -> do
     poke p (42::Int)
-    poke (add p 8) (88::Int)
+    poke (add p sInt) (88::Int)
   withForeignPtr fp $ \ p -> do
     gc
     peek p >>= print
-    peek (add p 8) >>= print
+    peek (add p sInt) >>= print
   let fp1 :: ForeignPtr Int
-      fp1 = plusForeignPtr fp 8
+      fp1 = plusForeignPtr fp sInt
   withForeignPtr fp1 $ \ p -> do
     peek p >>= print
   gc
