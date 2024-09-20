814d813eaf5a5f10c9df88e6336166c210632459
Lennart Augustsson <lennart@augustsson.net>
2024-09-20 11:41:36 +0200

    More tests.

--- tests/Arith.hs
+++ tests/Arith.hs
@@ -1,8 +1,12 @@
 module Arith(module Arith) where
 import Prelude
 
+vals :: [Int]
+vals = [-5, -2, -1, 0, 1, 2, 5]
+
 main :: IO ()
 main = do
-  putStrLn $ show [ op x y | x <- [0 - 5,0 - 2,0 - 1,0,1,2,5]::[Int], y <- [0 - 5,0 - 2,0 - 1,0,1,2,5::Int], op <- [(+),( - ),(*)] ]
-  putStrLn $ show [ op x y | x <- [0 - 5,0 - 2,0 - 1,0,1,2,5]::[Int], y <- [0 - 5,0 - 2,0 - 1,1,2,5::Int], op <- [quot, rem] ]
-  putStrLn $ show [ op x y | x <- [0 - 5,0 - 2,0 - 1,0,1,2,5]::[Int], y <- [0 - 5,0 - 2,0 - 1,0,1,2,5::Int], op <- [(==),(/=),(<),(<=),(>),(>=)] ]
+  putStrLn $ show [ op x y | x <- vals, y <- vals, op <- [(+),( - ),(*)] ]
+  putStrLn $ show [ op x y | x <- vals, y <- vals, y /= 0, op <- [quot, rem] ]
+  putStrLn $ show [ op x y | x <- vals, y <- vals, op <- [(==),(/=),(<),(<=),(>),(>=)] ]
+  putStrLn $ show [ op x y | x <- vals, y <- vals, let op = compare ]
