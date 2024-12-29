c6e8e33dc429495ac8a20e9a5d40f4ea2ceb787a
Lennart Augustsson <lennart@augustsson.net>
2024-12-29 12:41:26 +0100

    Test coerce

--- tests/Coerce.hs
+++ tests/Coerce.hs
@@ -13,10 +13,14 @@ toInt = coerce
 toInts :: (Coercible a Int) => [a] -> [Int]
 toInts = coerce
 
+casbs :: (Coercible a b) => [a] -> [b]
+casbs = coerce
+
 main :: IO ()
 main = do
   print (toInt (NT 5))
   print (toInts [NT 1, NT 2] :: [Int])
+  print (casbs [NT 1, NT 2, NT 3] :: [Int])
   print (coerce (List [3::Int, 4]) :: [Int])
   print (coerce (List [NT 5, NT 6]) :: [NT])
   print (coerce (List [NT 7, NT 8]) :: [Int])
