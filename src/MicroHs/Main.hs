d952f669554a3085ef7e233546ee51f4341f9fd3
Lennart Augustsson <lennart@augustsson.net>
2025-02-19 19:43:07 +1300

    Change the help message to a String literal trhe way I prefer.

--- src/MicroHs/Main.hs
+++ src/MicroHs/Main.hs
@@ -40,6 +40,7 @@ main = do
   dataDir <- getDataDir
   case args of
     ["-h"] -> putStrLn usage
+    ["-?"] -> putStrLn usage
     ["--help"] -> putStrLn longUsage
     ["--version"] -> putStrLn $ "MicroHs, version " ++ mhsVersion ++ ", combinator file version " ++ combVersion
     ["--numeric-version"] -> putStrLn mhsVersion
@@ -65,46 +66,47 @@ main = do
                   _   -> error usage
 
 usage :: String
-usage = "Usage: mhs [-h] [--help] [--version] [--numeric-version] [-v] [-q] [-l] [-s] [-r] [-C[R|W]] [-XCPP] [-DDEF] [-IPATH] [-T] [-z] [-iPATH] [-oFILE] [-a[PATH]] [-L[PATH|PKG]] [-PPKG] [-Q PKG [DIR]] [-tTARGET] [-optc OPTION] [-ddump-PASS] [MODULENAME..|FILE]"
+usage = "Usage: mhs [-h|?] [--help] [--version] [--numeric-version] [-v] [-q] [-l] [-s] [-r] [-C[R|W]] [-XCPP] [-DDEF] [-IPATH] [-T] [-z] [-iPATH] [-oFILE] [-a[PATH]] [-L[PATH|PKG]] [-PPKG] [-Q PKG [DIR]] [-tTARGET] [-optc OPTION] [-ddump-PASS] [MODULENAME..|FILE]"
 
 longUsage :: String
-longUsage = usage ++ "\nOptions:\n" ++ unlines details
+longUsage = usage ++ "\nOptions:\n" ++ details
   where
-    details =
-      [ "-h                 Print usage"
-      , "--help             Print this message"
-      , "--version          Print the version"
-      , "--numeric-version  Print the version number"
-      , "-v                 Increase verbosity (flag can be repeated)"
-      , "-q                 Decrease verbosity (flag can be repeated)"
-      , "-l                 Show every time a module is loaded"
-      , "-s                 Show compilation speed in lines/s"
-      , "-r                 Run directly"
-      , "-CR                Read compilation cache"
-      , "-CW                Write compilation cache"
-      , "-C                 Read and write compilation cache"
-      , "-XCPP              Run cpphs on source files"
-      , "-Dxxx              Pass -Dxxx to cpphs"
-      , "-Ixxx              Pass -Ixxx to cpphs"
-      , "-T                 Generate dynamic function usage statistics"
-      , "-z                 Compress combinator code generated in the .c file"
-      , "-iPATH             Add PATH to module search path"
-      , "-oFILE             Output to FILE"
-      , "                   If FILE ends in .comb produce a combinator file"
-      , "                   If FILE ends in .c produce a C file"
-      , "                   Otherwise compile the combinators together with the runtime system to produce a regular executable"
-      , "-a                 Clear package search path"
-      , "-aPATH             Add PATH to package search path"
-      , "-LPKG              List all modules of package PKG"
-      , "-PPKG              Build package PKG"
-      , "-Q PKG [DIR]       Install package PKG"
-      , "-tTARGET           Select target"
-      , "                   Distributed targets: default, emscripten"
-      , "                   Targets can be defined in targets.conf"
-      , "-optc OPTION       Options for the C compiler"
-      , "-ddump-PASS        Debug, print AST after PASS"
-      , "                   Possible passes: parse, typecheck, desugar, toplevel, combinator, all"
-      ]
+    details = "\
+      \-h                 Print usage\n\
+      \-?                 Print usage\n\
+      \--help             Print this message\n\
+      \--version          Print the version\n\
+      \--numeric-version  Print the version number\n\
+      \-v                 Increase verbosity (flag can be repeated)\n\
+      \-q                 Decrease verbosity (flag can be repeated)\n\
+      \-l                 Show every time a module is loaded\n\
+      \-s                 Show compilation speed in lines/s\n\
+      \-r                 Run directly\n\
+      \-CR                Read compilation cache\n\
+      \-CW                Write compilation cache\n\
+      \-C                 Read and write compilation cache\n\
+      \-XCPP              Run cpphs on source files\n\
+      \-Dxxx              Pass -Dxxx to cpphs\n\
+      \-Ixxx              Pass -Ixxx to cpphs\n\
+      \-T                 Generate dynamic function usage statistics\n\
+      \-z                 Compress combinator code generated in the .c file\n\
+      \-iPATH             Add PATH to module search path\n\
+      \-oFILE             Output to FILE\n\
+      \                   If FILE ends in .comb produce a combinator file\n\
+      \                   If FILE ends in .c produce a C file\n\
+      \                   Otherwise compile the combinators together with the runtime system to produce a regular executable\n\
+      \-a                 Clear package search path\n\
+      \-aPATH             Add PATH to package search path\n\
+      \-LPKG              List all modules of package PKG\n\
+      \-PPKG              Build package PKG\n\
+      \-Q PKG [DIR]       Install package PKG\n\
+      \-tTARGET           Select target\n\
+      \                   Distributed targets: default, emscripten\n\
+      \                   Targets can be defined in targets.conf\n\
+      \-optc OPTION       Options for the C compiler\n\
+      \-ddump-PASS        Debug, print AST after PASS\n\
+      \                   Possible passes: parse, typecheck, desugar, toplevel, combinator, all\n\
+      \"
 
 decodeArgs :: Flags -> [String] -> [String] -> (Flags, [String], [String])
 decodeArgs f mdls [] = (f, mdls, [])
