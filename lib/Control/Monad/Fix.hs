4f121f5d7e92d2bacbd56c5f3c57bc7ec54d00d1
Lennart Augustsson <lennart@augustsson.net>
2025-03-06 21:17:42 +0100

    Add some instances.

--- lib/Control/Monad/Fix.hs
+++ lib/Control/Monad/Fix.hs
@@ -4,12 +4,13 @@ module Control.Monad.Fix(
   ) where
 import Control.Monad
 import Data.Function(fix)
+import Data.Functor.Identity
 import Data.List.NonEmpty(NonEmpty(..))
 import Data.Monoid.Internal
 import Data.Tuple
 
 class (Monad m) => MonadFix m where
-        mfix :: (a -> m a) -> m a
+  mfix :: (a -> m a) -> m a
 
 {-
 instance MonadFix Solo where
@@ -37,14 +38,17 @@ instance MonadFix NonEmpty where
 --    mfix = fixIO
 
 instance MonadFix ((->) r) where
-    mfix f = \ r -> let a = f a r in a
+  mfix f = \ r -> let a = f a r in a
 
-{-
+instance MonadFix Identity where
+  mfix f = let a = f (runIdentity a) in a
+
 instance MonadFix (Either e) where
     mfix f = let a = f (unRight a) in a
              where unRight (Right x) = x
                    unRight (Left  _) = error "mfix Either: Left"
 
+{-
 instance MonadFix (ST s) where
         mfix = fixST
 
