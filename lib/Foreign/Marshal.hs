b039604c59aca409b0c466d4f51246737ac403f4
konsumlamm <konsumlamm@gmail.com>
2025-04-20 03:09:40 +0200

    Make `Foreign.Marshal` & `Foreign.Marshal.Error` report compliant

--- lib/Foreign/Marshal.hs
+++ lib/Foreign/Marshal.hs
@@ -2,12 +2,16 @@ module Foreign.Marshal(
   module Foreign.Marshal.Alloc,
   module Foreign.Marshal.Array,
   module Foreign.Marshal.Error,
---  module Foreign.Marshal.Pool,
-  module Foreign.Marshal.Utils
+  module Foreign.Marshal.Utils,
+  unsafeLocalState,
   ) where
+
+import Primitives (primPerformIO)
 
 import Foreign.Marshal.Alloc
 import Foreign.Marshal.Array
 import Foreign.Marshal.Error
---import Foreign.Marshal.Pool
 import Foreign.Marshal.Utils
+
+unsafeLocalState :: IO a -> a
+unsafeLocalState = primPerformIO
