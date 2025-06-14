3795c45e2634944b91e00613e681f14c2f2801f1
Lennart Augustsson <lennart@augustsson.net>
2025-06-14 23:36:09 +0200

    Add a hFlush

--- lib/Debug/Trace.hs
+++ lib/Debug/Trace.hs
@@ -1,12 +1,13 @@
 module Debug.Trace(module Debug.Trace) where
 import qualified Prelude(); import MiniPrelude
-import System.IO.Base(hPutStrLn, stderr)
+import System.IO.Base(hPutStrLn, stderr, hFlush)
 import Primitives
 
 trace :: forall a . String -> a -> a
 trace msg a = _primitive "IO.performIO" (
   do
     hPutStrLn stderr msg
+    hFlush stderr
     return a
   )
 
