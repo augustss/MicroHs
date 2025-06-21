cdec63c4ad9a6735e4d57d72cbd611dc49054a43
Tristan de Cacqueray <tristan@midirus.com>
2025-06-21 21:02:18 +0200

    Render foreign export header stub
    This change enables foreign code to import a Module_stub.h file.

--- tests/ForExp.c
+++ tests/ForExp.c
@@ -1,11 +1,4 @@
-#include <stdint.h>
-#include <stdio.h>
-
-typedef intptr_t value_t;
-void mhs_init(void);
-value_t funcName(value_t x1);
-value_t other(value_t x1, value_t x2);
-
+#include "ForExp_stub.h"
 
 int
 main(int argc, char **argv)
