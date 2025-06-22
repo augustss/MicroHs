72e2f7ffed98789f26e26d2620ad5f9bc718a6c5
Lennart Augustsson <lennart@augustsson.net>
2025-06-22 23:33:43 +0200

    Add JS test

---
+++ tests/ForImpJS.hs
@@ -0,0 +1,11 @@
+module ForImpJS where
+import Data.Text
+
+foreign import javascript "console.log('log: ' + UTF8ToString($0))" clog :: Text -> IO ()
+foreign import javascript "$1 + $2"                                  add :: Int -> Int -> Int
+
+main :: IO ()
+main = do
+  clog "JS log"
+  clog "JS log again"
+  clog $ pack $ show $ add 3 4
