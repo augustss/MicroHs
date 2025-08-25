c1070f47d073d5388c8f8596ce7e7c5b4edf64b4
konsumlamm <konsumlamm@gmail.com>
2025-08-25 18:24:19 +0200

    Modify `fromRat'`
    Remove `integerLog2`
    Add shortcut for small numerators & denominators

--- lib/Numeric.hs
+++ lib/Numeric.hs
@@ -57,52 +57,38 @@ fromRat (n :% d) | n > 0     = fromRat' (n :% d)
                  | n < 0     = - fromRat' ((-n) :% d)
                  | otherwise = encodeFloat 0 0             -- Zero
 
--- Conversion process:
--- Scale the rational number by the RealFloat base until
--- it lies in the range of the mantissa (as used by decodeFloat/encodeFloat).
--- Then round the rational to an Integer and encode it with the exponent
--- that we got from the scaling.
--- To speed up the scaling process we compute the log2 of the number to get
--- a first guess of the exponent.
-
 fromRat' :: (RealFloat a) => Rational -> a
 -- Invariant: argument is strictly positive
 fromRat' x = r
-  where b = floatRadix r
-        p = floatDigits r
-        (minExp0, _) = floatRange r
-        minExp = minExp0 - p            -- the real minimum exponent
-        xMax   = toRational (b ^ p)
-        ln     = integerLogBase b (numerator x)
-        ld     = integerLogBase b (denominator x)
-        p0     = (ln - ld - p) `max` minExp
-        -- if x = n/d and ln = integerLogBase b n, ld = integerLogBase b d,
-        -- then b^(ln-ld-1) < x < b^(ln-ld+1)
-        f = if p0 < 0 then 1 :% (b ^ (-p0)) else (b ^ p0) :% 1
-        x0 = x / f
-        -- if ln - ld >= minExp0, then b^(p-1) < x0 < b^(p+1), so there's at most
-        -- one scaling step needed, otherwise, x0 < b^p and no scaling is needed
-        (x', p') = if x0 >= xMax then (x0 / toRational b, p0+1) else (x0, p0)
-        r = encodeFloat (round x') p'
-
-wordLog2 :: Word -> Int
-wordLog2 w = _wordSize - 1 - primWordClz w
-
-integerLog2 :: Integer -> Int
-integerLog2 (I sign ds) =
-  case sign of
-    Minus -> 0 :: Int
-    Plus -> go ds 0
   where
-    go [] _ = 0 :: Int
-    go [d] i = wordLog2 d + (i `primIntShl` shiftD)
-    go (d : ds) i = go ds (i + 1)
+    n = numerator x
+    d = denominator x
+    b = floatRadix r
+    p = floatDigits r
+    (minExp, _) = floatRange r
+    maxVal = b ^ p
+    r =
+      if n < maxVal && d < maxVal then
+        -- no scaling needed
+        encodeFloat n 0 / encodeFloat d 0
+      else
+        let
+          ln = integerLogBase b n
+          ld = integerLogBase b d
+          -- b^(ln-ld-1) < x < b^(ln-ld+1)
+          p0 = max (ln - ld) minExp - p
+          f = if p0 < 0 then 1 :% (b ^ (-p0)) else (b ^ p0) :% 1
+          x0 = x / f
+          -- if ln - ld >= minExp, then b^(p-1) < x0 < b^(p+1), so there's at most
+          -- one scaling step needed, otherwise, x0 < b^p and no scaling is needed
+          (x', p') = if x0 >= toRational maxVal then (x0 / toRational b, p0 + 1) else (x0, p0)
+        in encodeFloat (round x') p'
 
+-- @integerLogBase b i@ is the logarithm of @i@ to base @b@, rounded down.
 integerLogBase :: Integer -> Integer -> Int
 integerLogBase b i
   | i == 0 = 0 :: Int
   | b < 2 = error "invalid base"
-  | b == 2 = integerLog2 i
   | otherwise = case go b of (_, e) -> e
   where
     go p = if i < p
