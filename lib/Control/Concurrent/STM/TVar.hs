3cb9a0f1e2cde7ccdec7d1fc388bfa130fd6accc
Lennart Augustsson <lennart@augustsson.net>
2025-09-06 20:14:10 +0200

    Temp

---
+++ lib/Control/Concurrent/STM/TVar.hs
@@ -0,0 +1,39 @@
+module Control.Concurrent.STM.TVar (
+        TVar,
+        newTVar,
+        newTVarIO,
+        readTVar,
+        readTVarIO,
+        writeTVar,
+        modifyTVar,
+        modifyTVar',
+        stateTVar,
+        swapTVar,
+  ) where
+
+modifyTVar :: TVar a -> (a -> a) -> STM ()
+modifyTVar var f = do
+    x <- readTVar var
+    writeTVar var (f x)
+
+modifyTVar' :: TVar a -> (a -> a) -> STM ()
+modifyTVar' var f = do
+    x <- readTVar var
+    writeTVar var $! f x
+
+stateTVar :: TVar s -> (s -> (a, s)) -> STM a
+stateTVar var f = do
+   s <- readTVar var
+   let !(a, s') = f s
+   writeTVar var s'
+   return a
+
+swapTVar :: TVar a -> a -> STM a
+swapTVar var new = do
+    old <- readTVar var
+    writeTVar var new
+    return old
+
+mkWeakTVar :: TVar a -> IO () -> IO (Weak (TVar a))
+mkWeakTVar t@(TVar t#) (IO finalizer) = IO $ \s ->
+    case mkWeak# t# t finalizer s of (# s1, w #) -> (# s1, Weak w #)
