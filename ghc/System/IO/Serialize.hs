cc9ba446582d724b2bf746b9f8c820c27c72de16
Lennart Augustsson <lennart@augustsson.net>
2025-09-11 15:08:03 -0400

    Add some funcions.

--- ghc/System/IO/Serialize.hs
+++ ghc/System/IO/Serialize.hs
@@ -1,25 +1,13 @@
 -- Copyright 2024 Lennart Augustsson
 -- See LICENSE file for full license.
 module System.IO.Serialize(
-{-
   hSerialize, hDeserialize,
-  writeSerialized,
--}
   writeSerialized,
   writeSerializedCompressed,
   readSerialized,
   ) where
---import System.IO
+import System.IO
 import GHC.Stack
-
-{-
-hSerialize   :: forall a . Handle -> a -> IO ()
-hSerialize = errghc
-hDeserialize :: forall a . Handle -> IO a
-hDeserialize = errghc
-writeSerialized :: forall a . FilePath -> a -> IO ()
-writeSerialized = errghc
--}
 
 writeSerializedCompressed :: forall a . HasCallStack => FilePath -> a -> IO ()
 writeSerializedCompressed = errghc
@@ -29,6 +17,12 @@ writeSerialized = errghc
 
 readSerialized :: forall a . HasCallStack => FilePath -> IO a
 readSerialized = errghc
+
+hSerialize   :: forall a . Handle -> a -> IO ()
+hSerialize = errghc
+
+hDeserialize :: forall a . Handle -> IO a
+hDeserialize = errghc
 
 errghc :: HasCallStack => a
 errghc = error "System.IO.System: serialization not available with ghc"
