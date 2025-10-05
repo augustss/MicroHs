83e6454c9315404cf3d868eb54ebebce857f981c
Lennart Augustsson <lennart@augustsson.net>
2025-10-05 11:18:34 +0200

    Add dummy 'lift' functions

---
+++ lib/Language/Haskell/TH/Quote.hs
@@ -0,0 +1,2 @@
+module Language.Haskell.TH.Quote(QuasiQuoter(..)) where
+data QuasiQuoter
--- lib/Language/Haskell/TH/Syntax.hs
+++ lib/Language/Haskell/TH/Syntax.hs
@@ -1,3 +1,5 @@
 -- This is a dummy module so deriving Lift can be spoofed.
-module Language.Haskell.TH.Syntax(Lift(..)) where
+module Language.Haskell.TH.Syntax(Lift(..), lift) where
 class Lift a
+lift :: a
+lift = error "Language.Haskell.TH.Syntax: not implemented"
