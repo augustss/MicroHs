1e9f539a9d0c840868d00bc1876d9f2553875bcd
Andrea Rossato <andrea.rossato@unitn.it>
2025-10-31 08:32:51 +0100

    add -optl for passing options to the linker

--- src/MicroHs/Main.hs
+++ src/MicroHs/Main.hs
@@ -72,7 +72,7 @@ main = do
                   _   -> mhsError usage
 
 usage :: String
-usage = "Usage: mhs [-h|?] [--help] [--version] [--numeric-version] [-v] [-q] [-l] [-s] [-r] [-C[R|W]] [-XCPP] [-DDEF] [-IPATH] [-T] [-z] [-b64] [-iPATH] [-oFILE] [-a[PATH]] [-L[FILE|PKG]] [-PPKG] [-Q PKG [DIR]] [-pFILE] [-tTARGET] [-optc OPTION] [-ddump-PASS] [MODULENAME..|FILE]"
+usage = "Usage: mhs [-h|?] [--help] [--version] [--numeric-version] [-v] [-q] [-l] [-s] [-r] [-C[R|W]] [-XCPP] [-DDEF] [-IPATH] [-T] [-z] [-b64] [-iPATH] [-oFILE] [-a[PATH]] [-L[FILE|PKG]] [-PPKG] [-Q PKG [DIR]] [-pFILE] [-tTARGET] [-optc OPTION] [-optl OPTION] [-ddump-PASS] [MODULENAME..|FILE]"
 
 longUsage :: String
 longUsage = usage ++ "\nOptions:\n" ++ details
@@ -113,6 +113,7 @@ longUsage = usage ++ "\nOptions:\n" ++ details
       \                   Distributed targets: default, emscripten, windows, tcc, environment\n\
       \                   Targets can be defined in targets.conf\n\
       \-optc OPTION       Options for the C compiler\n\
+      \-optl OPTION       Options passed by mhs to the C compiler for the linker\n\
       \--stdin            Use stdin in interactive system\n\
       \-ddump-PASS        Debug, print AST after PASS\n\
       \                   Possible passes: preproc, parse, derive, typecheck, desugar, toplevel, combinator, linked, all\n\
@@ -141,6 +142,8 @@ decodeArgs f mdls (arg:args) =
                 -> decodeArgs f{output = s} mdls args'
     "-optc" | s : args' <- args
                 -> decodeArgs f{cArgs = cArgs f ++ [s]} mdls args'
+    "-optl" | s : args' <- args
+                -> decodeArgs f{lArgs = lArgs f ++ [s]} mdls args'
     '-':'i':[]  -> decodeArgs f{paths = []} mdls args
     '-':'i':s   -> decodeArgs f{paths = paths f ++ [s]} mdls args
     '-':'o':s   -> decodeArgs f{output = s} mdls args
