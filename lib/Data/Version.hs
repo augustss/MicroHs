49ecc71ea5dd0cbb216cc4e254610244b18bd551
Lennart Augustsson <lennart@augustsson.net>
2025-11-16 23:33:09 +0100

    Update to include parseVersion

--- lib/Data/Version.hs
+++ lib/Data/Version.hs
@@ -1,11 +1,14 @@
 module Data.Version(
   Version(..),
   showVersion,
-  makeVersion
+  makeVersion,
+  parseVersion,
   ) where
 import qualified Prelude(); import MiniPrelude
 import Control.DeepSeq.Class
 import Data.List(intercalate)
+import Text.ParserCombinators.ReadP
+import Text.Read
 
 data Version = Version { versionBranch :: [Int], versionTags :: [String] }
   deriving (Show, Eq, Ord)
@@ -16,10 +19,9 @@ instance NFData Version where
 showVersion :: Version -> String
 showVersion (Version b _) = intercalate "." (map show b)
 
-{-
 parseVersion :: ReadP Version
 parseVersion = do branch <- sepBy1 (fmap read (munch1 isDigit)) (char '.')
-                  pure Version{versionBranch=branch}
--}
+                  pure Version{versionBranch=branch, versionTags=[]}
+
 makeVersion :: [Int] -> Version
 makeVersion b = Version b []
