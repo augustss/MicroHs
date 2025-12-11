2c9bf0c6ccb479d24375615c956edfce8b30f308
Lennart Augustsson <lennart@augustsson.net>
2025-12-12 00:50:30 +0100

    Add (deprecated) function system

--- lib/System/Process.hs
+++ lib/System/Process.hs
@@ -1,12 +1,22 @@
-module System.Process(callCommand) where
+module System.Process(callCommand, system) where
 import qualified Prelude(); import MiniPrelude
 import Control.Monad(when)
 import Foreign.C.String
+import System.Exit
 
 foreign import ccall "system" systemc :: CString -> IO Int
 
 callCommand :: String -> IO ()
 callCommand cmd = do
+  rc <- system cmd
+  case rc of
+    ExitSuccess -> return ()
+    ExitFailure r -> error $ "callCommand: failed " ++ show r ++ ", cmd=\n" ++ show cmd
+
+system :: String -> IO ExitCode
+system cmd = do
   r <- withCAString cmd systemc
-  when (r /= 0) $
-    error $ "callCommand: failed " ++ show r ++ ", " ++ show cmd
+  if (r == 0) then
+    return ExitSuccess
+   else
+    return (ExitFailure r)
