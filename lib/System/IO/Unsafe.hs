0772add6b20b64ba106d0679bfaba63529093e1e
konsumlamm <konsumlamm@gmail.com>
2025-12-14 19:35:20 +0100

    Add `unsafeDupablePerformIO` to `System.IO.Unsafe`

--- lib/System/IO/Unsafe.hs
+++ lib/System/IO/Unsafe.hs
@@ -1,9 +1,12 @@
-module System.IO.Unsafe(unsafePerformIO, unsafeInterleaveIO) where
+module System.IO.Unsafe(unsafePerformIO, unsafeDupablePerformIO, unsafeInterleaveIO) where
 import qualified Prelude()
 import Primitives
 
 unsafePerformIO :: IO a -> a
 unsafePerformIO = primPerformIO
 
-unsafeInterleaveIO :: forall a . IO a -> IO a
+unsafeDupablePerformIO :: IO a -> a
+unsafeDupablePerformIO = primPerformIO
+
+unsafeInterleaveIO :: IO a -> IO a
 unsafeInterleaveIO ioa = primReturn (primPerformIO ioa)
