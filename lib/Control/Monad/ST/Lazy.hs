6ec1c751419ba4ed72e5f311a86a766ea3b38f98
konsumlamm <konsumlamm@gmail.com>
2026-01-14 00:53:52 +0100

    Rewrite `Control.Monad.ST.Lazy`
    Add test from GHC's test suite

--- lib/Control/Monad/ST/Lazy.hs
+++ lib/Control/Monad/ST/Lazy.hs
@@ -8,37 +8,46 @@ module Control.Monad.ST.Lazy(
   RealWorld, stToIO,  -- GHC compat
   ) where
 import qualified Prelude(); import MiniPrelude
-import Primitives(primPerformIO, primReturn)
 import Control.Monad.Fix
 import Control.Monad.ST(RealWorld)
 import qualified Control.Monad.ST_Type as S
+import System.IO.Unsafe(unsafeInterleaveIO, unsafePerformIO)
 
-newtype ST s a = ST (IO a)
+newtype ST s a = ST { unST :: State s -> (a, State s) }
 
-unST :: ST s a -> IO a
-unST (ST io) = io
+data State s = S
 
 runST :: (forall s . ST s a) -> a
-runST (ST ioa) = primPerformIO ioa
+runST (ST st) = case st S of (r, _) -> r
 
 fixST :: (a -> ST s a) -> ST s a
-fixST f = ST (let x = primPerformIO (unST (f x)) in primReturn x)
+fixST f = ST $ \s -> let q@(r, _s') = unST (f r) s in q
 
 strictToLazyST :: S.ST s a -> ST s a
-strictToLazyST (S.ST io) = ST io
+strictToLazyST (S.ST io) = ST $ \s -> s `seq`
+  case unsafePerformIO (fmap MkSolo io) of
+    MkSolo a -> (a, s)
 
 lazyToStrictST :: ST s a -> S.ST s a
-lazyToStrictST (ST io) = S.ST io
+lazyToStrictST (ST st) = S.ST $ fmap id (case st S of (x, _) -> pure x)
 
 instance Functor (ST s) where
-  fmap f (ST x) = ST (primReturn (f (primPerformIO x)))
+  fmap f m = ST $ \s ->
+    let (r, s') = unST m s
+    in (f r, s')
 
 instance Applicative (ST s) where
-  pure x = ST (primReturn x)
-  ST f <*> ST x = ST (primReturn (primPerformIO f (primPerformIO x)))
+  pure x = ST $ \s -> (x, s)
+  liftA2 f m n = ST $ \s ->
+    let
+      (x, s') = unST m s
+      (y, s'') = unST n s'
+    in (f x y, s'')
 
 instance Monad (ST s) where
-  ST x >>= f = f (primPerformIO x)
+  m >>= k = ST $ \s ->
+    let (r, s') = unST m s
+    in unST (k r) s'
 
 instance MonadFix (ST s) where
   mfix = fixST
@@ -48,4 +57,4 @@ instance MonadFix (ST s) where
 -- but to be compatible we do it the same way.
 
 stToIO :: ST RealWorld a -> IO a
-stToIO = unST
+stToIO = S.unST . lazyToStrictST
