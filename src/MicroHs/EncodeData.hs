1157cbf83b073d33a00a59009eae02c71a691561
Lennart Augustsson <lennart@augustsson.net>
2026-01-24 23:28:52 +0100

    Beginning of new data type encoding.

--- src/MicroHs/EncodeData.hs
+++ src/MicroHs/EncodeData.hs
@@ -22,40 +22,50 @@ data SPat = SPat Con [Ident]    -- simple pattern
 --  deriving(Show, Eq)
 
 encCase :: Exp -> [(SPat, Exp)] -> Exp -> Exp
-encCase var pes dflt | n <= scottLimit = encCaseScott var pes dflt
-                     | otherwise = encCaseNo var pes dflt
+encCase var pes dflt | useDirect n (length pes) = encCaseDirect var pes dflt
+                     | otherwise                = encCaseNo var pes dflt
   where n = numConstr pes
 
+-- Encode constructor k of n, the number of arguments
+-- is given by the length of the list
 encConstr :: Int -> Int -> [Bool] -> Exp
-encConstr i n ss | n <= scottLimit = encConstrScott i n ss
-                 | otherwise       = encConstrNo i n ss
+encConstr k n ss = encConstrNo k n ss
+
+-- Decide if we are doing a direct dispatch or a comparison tree.
+useDirect :: Int -> Int -> Bool
+useDirect numCon numArm = numCon < 5            -- few constructors
+                       || numArm * 3 > numCon   -- more than 1/3 of the arms are present
 
 encIf :: Exp -> Exp -> Exp -> Exp
-encIf = encIfScott
+encIf c t e = app2 c e t
 
--- Lowest value that works is 3.
--- The runtime system knows the encoding of some types:
--- Bool, [], Ordering
-scottLimit :: Int
-scottLimit = 5
--- Some timing for different limits
---   3  27.9s
---   5  26.5s
---  10  26.4s
---  20  27.3s
--- 100  32.0s
+encList :: [Exp] -> Exp
+encList = foldr (app2 cCons) cNil
+
+cCons :: Exp
+cCons = encConstr 1 2 [False, False]
+
+cNil :: Exp
+cNil = encConstr 0 2 []
 
 -------------------------------------------
 
--- Scott encoding
---   C_i e1 ... en
--- encodes as, assuming k constructors
---   \ x1 ... xn -> \ f1 ... fi ... fk -> fi x1 ... xn
+encConstrNo :: Int -> Int -> [Bool] -> Exp
+encConstrNo i n ss =
+  let
+    xs = [mkIdent ("$x" ++ show j) | (j, _) <- zip [0::Int ..] ss]
+    strict (False:ys) (_:is) e = strict ys is e
+    strict (True:ys)  (x:is) e = app2 (Lit (LPrim "seq")) (Var x) (strict ys is e)
+    strict _ _ e = e
+    con = Lit $ LPrim $ "M" ++ show i ++ "_" ++ show n ++ "_" ++ show (length ss)
+  in
+    if all not ss then
+      con                   -- just an optimization to avoid a lot of eta reductions
+    else
+      lams xs $ strict ss xs $ apps con (map Var xs)
 
--- Encode a case expression:
---  case var of p1->e1; p2->e2; ...; _->dflt
-encCaseScott :: Exp -> [(SPat, Exp)] -> Exp -> Exp
-encCaseScott var pes dflt =
+encCaseDirect :: Exp -> [(SPat, Exp)] -> Exp -> Exp
+encCaseDirect var pes dflt =
   --trace ("mkCase " ++ show pes) $
   case pes of
     (SPat (ConData cs _ _) _, _) : _ ->
@@ -66,74 +76,21 @@ encCaseScott var pes dflt =
       in  apps var (map arm cs)
     _ -> undefined
 
--- Encode a constructor with strictness flags ss.
--- The constructor arity is given by ss, and the constructor number is i out of n.
-encConstrScott :: Int -> Int -> [Bool] -> Exp
-encConstrScott i n ss =
-  let
-    f = mkIdent "$f"
-    fs = map (\ k -> if k == i then f else dummyIdent) [0::Int .. n-1]
-    xs = [mkIdent ("$x" ++ show j) | (j, _) <- zip [0::Int ..] ss]
-    strict (False:ys) (_:is) e = strict ys is e
-    strict (True:ys)  (x:is) e = app2 (Lit (LPrim "seq")) (Var x) (strict ys is e)
-    strict _ _ e = e
-  in lams xs $ strict ss xs $ lams fs $ apps (Var f) (map Var xs)
-
-encIfScott :: Exp -> Exp -> Exp -> Exp
-encIfScott c t e = app2 c e t
-
-encList :: [Exp] -> Exp
-encList = foldr (app2 cCons) cNil
-
--- XXX could use encConstr
-cCons :: Exp
-cCons = Lit (LPrim "O")
-
--- XXX could use encConstr
-cNil :: Exp
-cNil = Lit (LPrim "K")
-
--------------------------------------------
-
--- Explicit constructor number encoding
---   C_i e1 e2 ... en
--- encodes as
---   (i, (e1, e2, ..., en))
--- with the tuples being Scott encoded.
-
--- Could be improved by a new node type holding
--- both the constructor number and a pointer to the tuple.
--- Also, could use a "jump table" case combinator instead
--- of a decision tree.
-
-encConstrNo :: Int -> Int -> [Bool] -> Exp
-encConstrNo i _n ss =
-  let
-    xs = [mkIdent ("$x" ++ show j) | (j, _) <- zip [0::Int ..] ss]
-    strict (False:ys) (_:is) e = strict ys is e
-    strict (True:ys)  (x:is) e = app2 (Lit (LPrim "seq")) (Var x) (strict ys is e)
-    strict _ _ e = e
-  in lams xs $ strict ss xs $ tuple [Lit (LInt i), tuple (map Var xs)]
-
-tuple :: [Exp] -> Exp
-tuple es = Lam f $ apps (Var f) es
-  where f = -- newIdent "$t" es --
-            mkIdent "$t"
-
 encCaseNo :: Exp -> [(SPat, Exp)] -> Exp -> Exp
-encCaseNo var pes dflt =
-  App var (Lam n $ Lam t $ caseTree (Var n) (Var t) 0 (numConstr pes) pes' dflt)
+encCaseNo var@(Var _) pes dflt =
+  App (Lam n $ caseTree (Var n) var 0 (numConstr pes) pes' dflt) (App getConNo var)
   where n = mkIdent "$n"
-        t = mkIdent "$tt"
         pes' = sortBy (\ (x, _, _) (y, _, _) -> compare x y)
                       [(conNo c, xs, e) | (SPat c xs, e) <- pes]
+        getConNo = Lit $ LPrim "cno"
+encCaseNo _ _ _ = undefined  -- this shouldn't happen
 
 caseTree :: Exp -> Exp -> Int -> Int -> [(Int, [Ident], Exp)] -> Exp -> Exp
-caseTree n tup lo hi pes dflt =
+caseTree n var lo hi pes dflt =
   case pes of
     [] -> dflt
-    [(i, xs, e)] | hi - lo == 1 -> match tup xs e
-                 | otherwise    -> encIf (eqInt n i) (match tup xs e) dflt
+    [(i, xs, e)] | hi - lo == 1 -> match var xs e
+                 | otherwise    -> encIf (eqInt n i) (match var xs e) dflt
 {-
     -- Strangely, this slows things down.
     -- Why?  A 3-way branch should be better than a 2-way.
@@ -147,7 +104,7 @@ caseTree n tup lo hi pes dflt =
     _ ->
       case splitAt (length pes `quot` 2) pes of
         (pesl, pesh@((i, _, _):_)) ->
-          encIf (ltInt n i) (caseTree n tup lo i pesl dflt) (caseTree n tup i hi pesh dflt)
+          encIf (ltInt n i) (caseTree n var lo i pesl dflt) (caseTree n var i hi pesh dflt)
         _ -> impossible
  where
    eqInt :: Exp -> Int -> Exp
@@ -155,7 +112,8 @@ caseTree n tup lo hi pes dflt =
    ltInt :: Exp -> Int -> Exp
    ltInt x i = app2 (Lit (LPrim "<")) x (Lit (LInt i))
    match :: Exp -> [Ident] -> Exp -> Exp
-   match e is rhs = App e $ lams is rhs
+   match e is rhs = App (App (unpack (length is)) e) (lams is rhs)
+   unpack l = Lit $ LPrim $ "U" ++ show l
 {-
    cmpInt :: Exp -> Int -> Exp
    cmpInt x i = app2 (Lit (LPrim "icmp")) x (Lit (LInt i))
