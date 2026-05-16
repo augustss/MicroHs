7e74032c8a184a969c42b04685bfac49ba839f4f
Lennart Augustsson <lennart@augustsson.net>
2026-05-16 23:16:13 +0200

    Handle snprintf with old MSVC

--- src/runtime/windows/config.h
+++ src/runtime/windows/config.h
@@ -101,6 +101,10 @@
     #include <inttypes.h>
 #endif  /* defined(_MSC_VER) && (_MSC_VER < 1800) */
 
+#if defined(_MSC_VER) && _MSC_VER < 1900
+    #define snprintf _snprintf
+#endif
+
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <intrin.h>
