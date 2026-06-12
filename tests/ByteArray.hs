e2ad62d5b15777fc47ad69c6dc15b52c3a3447fd
Lennart Augustsson <lennart@augustsson.net>
2026-06-13 00:53:20 +0200

    New primops for dealing with mutable bytearrays.
    Each byte array now carries a size as well as a capacity
    (i.e., the size of the allocated memory).
    It is now relatively cheap to append a byte to a bytearray.

--- tests/ByteArray.hs
+++ tests/ByteArray.hs
@@ -4,9 +4,23 @@ import Data.Array.Byte
 
 main :: IO ()
 main = do
-  let b = runST $ do
+  let b1 = runST $ do
         a <- newMutableByteArray 5
         writeWord8 a 1 65
         writeWord8 a 3 66
+        c <- readWord8 a 1
+        writeWord8 a 4 c
         freezeMutableByteArray a
-  print b
+  print b1
+
+  let b2 = runST $ do
+        a <- newMutableEmptyByteArray 2
+        appendByte a 110
+        appendByte a 111
+        appendByte a 112
+        appendByte a 113
+        appendByte a 114
+        appendChar a 'A'
+        appendChar a 'å'
+        freezeMutableByteArray a
+  print b2
