2710eb553404334f404b18260662b737e872d8c3
Lennart Augustsson <lennart@augustsson.net>
2026-06-19 00:34:38 +0200

    Change representation for MD5 checksums.
    Show be portable between 32 and 64 bits now.

--- lib/System/IO/MD5.hs
+++ lib/System/IO/MD5.hs
@@ -1,48 +1,50 @@
--- Copyright 2023 Lennart Augustsson
+-- Copyright 2023-2026 Lennart Augustsson
 -- See LICENSE file for full license.
 module System.IO.MD5(MD5CheckSum, md5File, md5Handle, md5String, md5Combine) where
 import qualified Prelude(); import MiniPrelude
 import Primitives(primPerformIO)
 import Control.DeepSeq.Class
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Unsafe as BS
 import Data.Coerce
-import Data.Word(Word32)
+import Data.Word(Word8)
 import Foreign.C.String
 import Foreign.Marshal.Alloc
-import Foreign.Marshal.Array
+import Foreign.Marshal.Array(withArrayLen)
+import Foreign.Marshal.Utils(copyBytes)
 import Foreign.Ptr
+import Foreign.Storable
 import System.IO
 import System.IO.Internal
 
--- XXX change this to use hex bytestrings instead.
+foreign import ccall "md5BFILE"  c_md5BFILE  :: Ptr BFILE        -> Ptr MD5CheckSum -> IO ()
+foreign import ccall "md5String" c_md5String :: CString          -> Ptr MD5CheckSum -> IO ()
+foreign import ccall "md5Array"  c_md5Array  :: Ptr MD5CheckSum  -> Ptr MD5CheckSum -> Int -> IO ()
 
-foreign import ccall "md5BFILE"  c_md5BFILE  :: Ptr BFILE  -> Ptr Word32 -> IO ()
-foreign import ccall "md5String" c_md5String :: CString    -> Ptr Word32 -> IO ()
-foreign import ccall "md5Array"  c_md5Array  :: Ptr Word32 -> Ptr Word32 -> Int -> IO ()
-
-newtype MD5CheckSum = MD5 [Word32]  -- 4*32 bits
+newtype MD5CheckSum = MD5 BS.ByteString  -- 16 bytes
   deriving (Eq, Ord)
 
 instance Show MD5CheckSum where
-  show (MD5 ws) = "MD5" ++ show ws
+  showsPrec p (MD5 bs) = showParen (p > 10) $ showString "MD5 " . showString (concatMap hex (BS.unpack bs))
+    where hex b = [intToDigit q, intToDigit r] where (q, r) = quotRem (fromIntegral b) 16
 
 instance NFData MD5CheckSum where
-  rnf (MD5 ws) = loop ws
-    where loop [] = ()
-          loop (a:as) = a `seq` loop as
+  rnf (MD5 bs) = seq bs ()
+
+instance Storable MD5CheckSum where
+  sizeOf _ = md5Len
+  alignment _ = 1
+  peek p = MD5 <$> BS.packCStringLen (castPtr p, md5Len)
+  poke p (MD5 bs) = BS.unsafeUseAsCString bs $ \ cp -> copyBytes p (castPtr cp) md5Len
 
 md5Len :: Int
 md5Len = 16   -- The MD5 checksum is 16 bytes
 
-md5WLen :: Int  -- length in Word32
-md5WLen = md5Len `quot` 4
-
-chksum :: (Ptr Word32 -> IO ()) -> IO MD5CheckSum
-chksum fn = do
-  buf <- mallocArray md5WLen
-  fn buf
-  wmd5 <- peekArray md5WLen buf
-  free buf
-  return (MD5 wmd5)
+chksum :: (Ptr MD5CheckSum -> IO ()) -> IO MD5CheckSum
+chksum fn =
+  alloca $ \ buf -> do
+    fn buf
+    peek buf
 
 md5String :: String -> MD5CheckSum
 md5String s = primPerformIO $ withCAString s $ chksum . c_md5String
@@ -64,5 +66,5 @@ md5Combine :: [MD5CheckSum] -> MD5CheckSum
 md5Combine [] = error "md5Combine: empty"
 md5Combine [m] = m
 md5Combine ms = primPerformIO $
-  withArray [ w | MD5 ws <- ms, w <- ws ] $ \ a ->
-    chksum $ \ w -> c_md5Array a w (length ms * md5Len)
+  withArrayLen ms $ \ l a ->
+    chksum $ \ w -> c_md5Array a w (l * md5Len)
