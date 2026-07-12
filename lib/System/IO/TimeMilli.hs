b93fbf0ff947c3b9c7973b0816080a9a13efc4d0
Lennart Augustsson <lennart@augustsson.net>
2026-07-13 00:44:00 +0200

    Add getBootTimeMicro

--- lib/System/IO/TimeMilli.hs
+++ lib/System/IO/TimeMilli.hs
@@ -1,10 +1,14 @@
-module System.IO.TimeMilli(getTimeMicro, getTimeMilli) where
+module System.IO.TimeMilli(getTimeMicro, getTimeMilli, getBootTimeMicro) where
 import qualified Prelude(); import MiniPrelude
 
 foreign import ccall "GETTIMEMICRO" c_getTimeMicro :: IO Int
+foreign import ccall "GETBOOTTIMEMICRO" c_getBootTimeMicro :: IO Int
 
 getTimeMicro :: IO Int
 getTimeMicro = c_getTimeMicro
 
 getTimeMilli :: IO Int
 getTimeMilli = do t <- c_getTimeMicro; return (t `quot` 1000)
+
+getBootTimeMicro :: IO Int
+getBootTimeMicro = c_getBootTimeMicro
