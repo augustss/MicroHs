ab521d7676da640b23087e4d5bf2d58d92d0b209
Lennart Augustsson <lennart@augustsson.net>
2026-07-13 11:20:08 +0200

    Add new API functions

--- ghc/System/IO/TimeMilli.hs
+++ ghc/System/IO/TimeMilli.hs
@@ -1,7 +1,12 @@
-module System.IO.TimeMilli(getTimeMilli) where
+module System.IO.TimeMilli(getTimeMilli, getTimeMicro, getBootTimeMicro) where
 import Data.Time
 import Data.Time.Clock.POSIX
 
 getTimeMilli :: IO Int
 getTimeMilli  = floor . (1000 *) . nominalDiffTimeToSeconds . utcTimeToPOSIXSeconds <$> getCurrentTime
 
+getTimeMicro :: IO Int
+getTimeMicro  = floor . (1000000 *) . nominalDiffTimeToSeconds . utcTimeToPOSIXSeconds <$> getCurrentTime
+
+getBootTimeMicro :: IO Int
+getBootTimeMicro = return 0
